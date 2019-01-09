332ed8ee806f3527f58c8f520b943d8d87c3387f
Jamie Burns <1443443+fantasticjamieburns@users.noreply.github.com>
2019-01-09 14:05:45 +0000

    Infrastructure setup, with shared app insights

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,4 +1,7 @@
-provider "azurerm" {}
+# Temporary fix for template API version error on deployment
+provider "azurerm" {
+  version = "1.19.0"
+}
 
 locals {
 
@@ -22,22 +25,111 @@ data "azurerm_key_vault" "ia_key_vault" {
   resource_group_name = "${local.key_vault_name}"
 }
 
+data "azurerm_key_vault_secret" "test_caseofficer_username" {
+  name      = "test-caseofficer-username"
+  vault_uri = "${data.azurerm_key_vault.ia_key_vault.vault_uri}"
+}
+
+data "azurerm_key_vault_secret" "test_caseofficer_password" {
+  name      = "test-caseofficer-password"
+  vault_uri = "${data.azurerm_key_vault.ia_key_vault.vault_uri}"
+}
+
+data "azurerm_key_vault_secret" "test_law_firm_a_username" {
+  name      = "test-law-firm-a-username"
+  vault_uri = "${data.azurerm_key_vault.ia_key_vault.vault_uri}"
+}
+
+data "azurerm_key_vault_secret" "test_law_firm_a_password" {
+  name      = "test-law-firm-a-password"
+  vault_uri = "${data.azurerm_key_vault.ia_key_vault.vault_uri}"
+}
+
+data "azurerm_key_vault_secret" "system_username" {
+  name      = "system-username"
+  vault_uri = "${data.azurerm_key_vault.ia_key_vault.vault_uri}"
+}
+
+data "azurerm_key_vault_secret" "system_password" {
+  name      = "system-password"
+  vault_uri = "${data.azurerm_key_vault.ia_key_vault.vault_uri}"
+}
+
+data "azurerm_key_vault_secret" "idam_client_id" {
+  name      = "idam-client-id"
+  vault_uri = "${data.azurerm_key_vault.ia_key_vault.vault_uri}"
+}
+
+data "azurerm_key_vault_secret" "idam_secret" {
+  name      = "idam-secret"
+  vault_uri = "${data.azurerm_key_vault.ia_key_vault.vault_uri}"
+}
+
+data "azurerm_key_vault_secret" "idam_redirect_uri" {
+  name      = "idam-redirect-uri"
+  vault_uri = "${data.azurerm_key_vault.ia_key_vault.vault_uri}"
+}
+
+data "azurerm_key_vault_secret" "s2s_secret" {
+  name      = "s2s-secret"
+  vault_uri = "${data.azurerm_key_vault.ia_key_vault.vault_uri}"
+}
+
+data "azurerm_key_vault_secret" "s2s_microservice" {
+  name      = "s2s-microservice"
+  vault_uri = "${data.azurerm_key_vault.ia_key_vault.vault_uri}"
+}
+
+data "azurerm_key_vault_secret" "ccd_url" {
+  name      = "ccd-url"
+  vault_uri = "${data.azurerm_key_vault.ia_key_vault.vault_uri}"
+}
+
+data "azurerm_key_vault_secret" "dm_url" {
+  name      = "dm-url"
+  vault_uri = "${data.azurerm_key_vault.ia_key_vault.vault_uri}"
+}
+
+data "azurerm_key_vault_secret" "idam_url" {
+  name      = "idam-url"
+  vault_uri = "${data.azurerm_key_vault.ia_key_vault.vault_uri}"
+}
+
+data "azurerm_key_vault_secret" "s2s_url" {
+  name      = "s2s-url"
+  vault_uri = "${data.azurerm_key_vault.ia_key_vault.vault_uri}"
+}
+
 module "ia_case_documents_api" {
-  source              = "git@github.com:hmcts/cnp-module-webapp?ref=master"
-  product             = "${var.product}-${var.component}"
-  location            = "${var.location}"
-  env                 = "${var.env}"
-  ilbIp               = "${var.ilbIp}"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
-  subscription        = "${var.subscription}"
-  capacity            = "${var.capacity}"
-  instance_size       = "${var.instance_size}"
-  common_tags         = "${merge(var.common_tags, map("lastUpdated", "${timestamp()}"))}"
-  asp_name            = "${local.app_service_plan}"
-  asp_rg              = "${local.app_service_plan}"
+  source                          = "git@github.com:hmcts/cnp-module-webapp?ref=master"
+  product                         = "${var.product}-${var.component}"
+  location                        = "${var.location}"
+  env                             = "${var.env}"
+  ilbIp                           = "${var.ilbIp}"
+  resource_group_name             = "${azurerm_resource_group.rg.name}"
+  subscription                    = "${var.subscription}"
+  capacity                        = "${var.capacity}"
+  instance_size                   = "${var.instance_size}"
+  common_tags                     = "${merge(var.common_tags, map("lastUpdated", "${timestamp()}"))}"
+  appinsights_instrumentation_key = "${var.appinsights_instrumentation_key}"
+  asp_name                        = "${local.app_service_plan}"
+  asp_rg                          = "${local.app_service_plan}"
 
   app_settings = {
     LOGBACK_REQUIRE_ALERT_LEVEL = false
     LOGBACK_REQUIRE_ERROR_CODE  = false
+
+    IA_SYSTEM_USERNAME   = "${data.azurerm_key_vault_secret.system_username.value}"
+    IA_SYSTEM_PASSWORD   = "${data.azurerm_key_vault_secret.system_password.value}"
+    IA_IDAM_CLIENT_ID    = "${data.azurerm_key_vault_secret.idam_client_id.value}"
+    IA_IDAM_SECRET       = "${data.azurerm_key_vault_secret.idam_secret.value}"
+    IA_IDAM_REDIRECT_URI = "${data.azurerm_key_vault_secret.idam_redirect_uri.value}"
+    IA_S2S_SECRET        = "${data.azurerm_key_vault_secret.s2s_secret.value}"
+    IA_S2S_MICROSERVICE  = "${data.azurerm_key_vault_secret.s2s_microservice.value}"
+
+    CCD_URL  = "${data.azurerm_key_vault_secret.ccd_url.value}"
+    DM_URL   = "${data.azurerm_key_vault_secret.dm_url.value}"
+    IDAM_URL = "${data.azurerm_key_vault_secret.idam_url.value}"
+    S2S_URL  = "${data.azurerm_key_vault_secret.s2s_url.value}"
   }
 }
