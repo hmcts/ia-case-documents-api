56842dd7fc650eb73664e73b452d0d569c5719c8
Jamie Burns <1443443+fantasticjamieburns@users.noreply.github.com>
2019-01-15 11:16:33 +0000

    Removed access key log

--- src/main/java/uk/gov/hmcts/reform/iacasedocumentsapi/infrastructure/clients/DocmosisDocumentGenerator.java
+++ src/main/java/uk/gov/hmcts/reform/iacasedocumentsapi/infrastructure/clients/DocmosisDocumentGenerator.java
@@ -71,7 +71,6 @@ public class DocmosisDocumentGenerator implements DocumentGenerator {
 
             LOG.info("MediaType: " + MediaType.MULTIPART_FORM_DATA_VALUE);
             LOG.info("endpoint: " + docmosisUrl + docmosisRenderUri);
-            LOG.info("accessKey: " + docmosisAccessKey);
             LOG.info("outputName: " + fileNameWithExension);
             LOG.info("outputFormat: " + fileExtension.toLowerCase());
             LOG.info("templateName: " + templateName);
