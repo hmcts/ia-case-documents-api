9b7dd5279076fcac153c6e74a50ae19558e5e18d
raghera <raghera@hotmail.com>
2019-06-26 14:47:41 +0100

    RIA 1538 - Aks and helm charts (#48)
    * RIA 1538 AKS and Helm Charts
    
    * Adding liveness config
    
    * Adding bootstrap changes
    
    * Adding different docmosis keys

---
+++ src/functionalTest/java/uk/gov/hmcts/reform/iacasedocumentsapi/LivenessProbeTest.java
@@ -0,0 +1,46 @@
+package uk.gov.hmcts.reform.iacasedocumentsapi;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.restassured.RestAssured;
+import net.serenitybdd.rest.SerenityRest;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("functional")
+public class LivenessProbeTest {
+
+    @Value("${targetInstance}") private String targetInstance;
+
+    @Before
+    public void setUp() {
+        RestAssured.baseURI = targetInstance;
+        RestAssured.useRelaxedHTTPSValidation();
+    }
+
+    @Test
+    public void should_ping_liveness_endpoint_and_get_ok() {
+
+        String response = SerenityRest
+            .when()
+            .get("/health/liveness")
+            .then()
+            .statusCode(HttpStatus.OK.value())
+            .log().all()
+            .and()
+            .extract().body()
+            .jsonPath().get("status");
+
+        assertThat(response).isEqualTo("UP");
+
+    }
+
+}
