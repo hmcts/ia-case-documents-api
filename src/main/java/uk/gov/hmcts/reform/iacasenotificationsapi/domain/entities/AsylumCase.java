8c529b3ff9deb3732999fb7bb9f48c947c417c8e
makowalski <marcin.kowalski.it.consulting@gmail.com>
2019-09-20 11:38:04 +0100

    RIA-1654 admin edit listing (#70)

--- src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/entities/AsylumCase.java
+++ src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/entities/AsylumCase.java
@@ -2,8 +2,10 @@ package uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+
 import java.util.HashMap;
 import java.util.Optional;
+
 import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.ccd.CaseData;
 
 public class AsylumCase extends HashMap<String, Object> implements CaseData {
@@ -14,6 +16,13 @@ public class AsylumCase extends HashMap<String, Object> implements CaseData {
         objectMapper.registerModule(new Jdk8Module());
     }
 
+    /*
+    Security vulnerabilities (CVE-2019-14540, CVE-2019-16335) forced us to update jackson-core library to version: 2.10.0.pr3
+    That version has change in ObjectMapper API where <T> generic should be passed to "convertValue" method instead of taking any type <?>
+    Returning Object and casting to T ref is safe because we defined all types in AsylumCaseDefinition enum.
+    We cannot parametrized enum with T ref and we want to keep AsylumCaseDefinition as it is, that is why we need below workaround.
+    */
+    @SuppressWarnings("unchecked")
     public <T> Optional<T> read(AsylumCaseDefinition asylumCaseDefinition) {
 
         Object o = this.get(asylumCaseDefinition.value());
@@ -22,9 +31,9 @@ public class AsylumCase extends HashMap<String, Object> implements CaseData {
             return Optional.empty();
         }
 
-        T value = objectMapper.convertValue(o, asylumCaseDefinition.getTypeReference());
+        Object value = objectMapper.convertValue(o, asylumCaseDefinition.getTypeReference());
 
-        return Optional.of(value);
+        return Optional.of((T) value);
     }
 
     public <T> Optional<T> read(AsylumCaseDefinition asylumCaseDefinition, Class<T> type) {
