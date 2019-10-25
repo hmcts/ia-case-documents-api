fc1782aa3c2a77e603c0afc260f4755658e3b21d
Tony Mortimer <mrmort80@gmail.com>
2019-10-25 10:29:02 +0100

    Integration test refactor: imported test infrastructure (#67)
    Integration test refactor: imported test infrastructure
    
    Integration test refactor: imported test infrastructure
    
    Integration test refactor: imported test infrastructure
    
    Integration test refactor: imported test infrastructure

---
+++ src/integrationTest/java/uk/gov/hmcts/reform/iacasedocumentsapi/component/GenerateDecisionAndReasonsTest.java
@@ -0,0 +1,67 @@
+package uk.gov.hmcts.reform.iacasedocumentsapi.component;
+
+import static com.google.common.collect.Sets.newHashSet;
+import static org.assertj.core.api.Assertions.assertThat;
+import static uk.gov.hmcts.reform.iacasedocumentsapi.component.testutils.fixtures.AsylumCaseForTest.anAsylumCase;
+import static uk.gov.hmcts.reform.iacasedocumentsapi.component.testutils.fixtures.CallbackForTest.CallbackForTestBuilder.callback;
+import static uk.gov.hmcts.reform.iacasedocumentsapi.component.testutils.fixtures.CaseDetailsForTest.CaseDetailsForTestBuilder.someCaseDetailsWith;
+import static uk.gov.hmcts.reform.iacasedocumentsapi.component.testutils.fixtures.UserDetailsForTest.UserDetailsForTestBuilder.userWith;
+import static uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.AsylumCaseDefinition.*;
+import static uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.DocumentTag.*;
+import static uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.ccd.State.DECISION;
+
+import java.util.List;
+import java.util.Optional;
+import org.junit.Test;
+import uk.gov.hmcts.reform.iacasedocumentsapi.component.testutils.SpringBootIntegrationTest;
+import uk.gov.hmcts.reform.iacasedocumentsapi.component.testutils.fixtures.PreSubmitCallbackResponseForTest;
+import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.DocumentWithMetadata;
+import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.ccd.Event;
+import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.ccd.field.IdValue;
+import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.ccd.field.YesOrNo;
+
+public class GenerateDecisionAndReasonsTest extends SpringBootIntegrationTest {
+
+    @Test
+    public void generates_decision_and_reasons() {
+
+        given.someLoggedIn(userWith()
+            .roles(newHashSet("caseworker-ia", "caseworker-ia-caseofficer"))
+            .forename("Case")
+            .surname("Officer"));
+
+        given.docmosisWillReturnSomeDocument();
+        given.theDocoumentsManagementApiIsAvailable();
+
+        PreSubmitCallbackResponseForTest response = iaCaseDocumentsApiClient.aboutToSubmit(callback()
+            .event(Event.GENERATE_DECISION_AND_REASONS)
+            .caseDetails(someCaseDetailsWith()
+                .state(DECISION)
+                .caseData(anAsylumCase()
+                    .with(APPEAL_REFERENCE_NUMBER, "some-appeal-reference-number")
+                    .with(APPELLANT_FAMILY_NAME, "some-fname")
+                    .with(APPELLANT_GIVEN_NAMES, "some-gname")
+                    .with(APPEAL_REFERENCE_NUMBER, "some-appeal-reference-number")
+                    .with(CASE_INTRODUCTION_DESCRIPTION, "some-case-intro")
+                    .with(APPELLANT_CASE_SUMMARY_DESCRIPTION, "some-case-summary-description")
+                    .with(IMMIGRATION_HISTORY_AGREEMENT, YesOrNo.YES)
+                    .with(AGREED_IMMIGRATION_HISTORY_DESCRIPTION, "some-agreed-immigration-description")
+                    .with(SCHEDULE_OF_ISSUES_AGREEMENT, "Yes")
+                    .with(APPELLANTS_AGREED_SCHEDULE_OF_ISSUES_DESCRIPTION, "some-agreed-schedule-of-issues-description")
+                    .with(DECISION_AND_REASONS_AVAILABLE, YesOrNo.NO)
+                    .with(ANONYMITY_ORDER, YesOrNo.YES)
+                    .with(APPELLANT_REPRESENTATIVE, "ted")
+                    .with(RESPONDENT_REPRESENTATIVE, "bill")
+                    .with(LIST_CASE_HEARING_CENTRE, "taylorHouse")
+
+                )));
+
+        Optional<List<IdValue<DocumentWithMetadata>>> draftDecisionAndReasonsDocuments =
+            response.getAsylumCase().read(DRAFT_DECISION_AND_REASONS_DOCUMENTS);
+
+        IdValue<DocumentWithMetadata> documentWithMetadataIdValue = draftDecisionAndReasonsDocuments.get().get(0);
+
+        assertThat(draftDecisionAndReasonsDocuments.get().size()).isEqualTo(1);
+        assertThat(documentWithMetadataIdValue.getValue().getTag()).isEqualTo(DECISION_AND_REASONS_DRAFT);
+    }
+}
