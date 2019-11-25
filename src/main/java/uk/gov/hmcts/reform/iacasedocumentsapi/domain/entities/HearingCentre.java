8363249e33e3ab50a9fe4adfd6311266eee30ae2
Brad Sorour <48841470+bradsorour@users.noreply.github.com>
2019-11-25 16:32:05 +0000

    RIA-1976 add new hearing centres (#137)

--- src/main/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/entities/HearingCentre.java
+++ src/main/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/entities/HearingCentre.java
@@ -3,14 +3,20 @@ package uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities;
 import static java.util.Arrays.stream;
 
 import com.fasterxml.jackson.annotation.JsonValue;
+
 import java.util.Optional;
+
 
 public enum HearingCentre {
 
     BRADFORD("bradford"),
     MANCHESTER("manchester"),
     NEWPORT("newport"),
-    TAYLOR_HOUSE("taylorHouse");
+    TAYLOR_HOUSE("taylorHouse"),
+    NORTH_SHIELDS("northShields"),
+    BIRMINGHAM("birmingham"),
+    HATTON_CROSS("hattonCross"),
+    GLASGOW("glasgow");
 
     @JsonValue
     private final String value;
