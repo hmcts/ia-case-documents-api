56c15fe887d4739333590479bbed54b1d49385a5
tarun-palisetty <tarun.mca@gmail.com>
2020-02-28 18:12:08 +0000

    RIA-2646 log ccd id on exceptions

--- src/main/java/uk/gov/hmcts/reform/iacasedocumentsapi/infrastructure/controllers/advice/CallbackControllerAdvice.java
+++ src/main/java/uk/gov/hmcts/reform/iacasedocumentsapi/infrastructure/controllers/advice/CallbackControllerAdvice.java
@@ -4,11 +4,15 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 import javax.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import uk.gov.hmcts.reform.iacasedocumentsapi.domain.RequiredFieldMissingException;
 import uk.gov.hmcts.reform.iacasedocumentsapi.infrastructure.clients.DocumentServiceResponseException;
 import uk.gov.hmcts.reform.iacasedocumentsapi.infrastructure.clients.DocumentStitchingErrorResponseException;
@@ -16,7 +20,7 @@ import uk.gov.hmcts.reform.iacasedocumentsapi.infrastructure.clients.DocumentSti
 @Slf4j
 @ControllerAdvice(basePackages = "uk.gov.hmcts.reform.iacasedocumentsapi.infrastructure.controllers")
 @RequestMapping(produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
-public class CallbackControllerAdvice {
+public class CallbackControllerAdvice extends ResponseEntityExceptionHandler {
 
     private ErrorResponseLogger errorResponseLogger;
 
@@ -24,12 +28,18 @@ public class CallbackControllerAdvice {
         this.errorResponseLogger = errorResponseLogger;
     }
 
-    @ExceptionHandler(RequiredFieldMissingException.class)
-    protected ResponseEntity<String> handleRequiredFieldMissingException(
+    @ExceptionHandler({
+        IllegalStateException.class,
+        IllegalArgumentException.class,
+        RequiredFieldMissingException.class
+    })
+    protected ResponseEntity<String> handleExceptions(
         HttpServletRequest request,
         RequiredFieldMissingException e
     ) {
-        log.error("handling exception with message: {}", e.getMessage());
+        log.error("Exception for the CCDCaseId: {}",
+            RequestContextHolder.currentRequestAttributes().getAttribute("CCDCaseId", RequestAttributes.SCOPE_REQUEST));
+        ExceptionUtils.printRootCauseStackTrace(e);
 
         return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
@@ -39,8 +49,8 @@ public class CallbackControllerAdvice {
         HttpServletRequest request,
         DocumentStitchingErrorResponseException ex
     ) {
-        log.error("Exception when stitching a bundle with message: {}.", ex.getMessage());
-
+        log.error("Exception for the CCDCaseId: {}",
+            RequestContextHolder.currentRequestAttributes().getAttribute("CCDCaseId", RequestAttributes.SCOPE_REQUEST));
         errorResponseLogger.maybeLogErrorsListResponse(ex.getStitchingServiceResponse());
 
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
@@ -51,11 +61,10 @@ public class CallbackControllerAdvice {
         HttpServletRequest request,
         DocumentServiceResponseException ex
     ) {
-        log.error("Document service Exception with message: {}.", ex.getMessage());
-
+        log.error("Exception for the CCDCaseId: {}",
+            RequestContextHolder.currentRequestAttributes().getAttribute("CCDCaseId", RequestAttributes.SCOPE_REQUEST));
         errorResponseLogger.maybeLogException(ex.getCause());
 
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
-
 }
