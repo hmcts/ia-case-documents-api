66a6c9d1b6449f985a07e5ad631a4182feccf0e7
Lee365 <55591679+Lee365@users.noreply.github.com>
2020-04-03 10:40:00 +0100

    RIA-2821 urls out of vault (#237)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -235,11 +235,6 @@ data "azurerm_key_vault_secret" "idam_secret" {
   vault_uri = "${data.azurerm_key_vault.ia_key_vault.vault_uri}"
 }
 
-data "azurerm_key_vault_secret" "idam_redirect_uri" {
-  name      = "idam-redirect-uri"
-  vault_uri = "${data.azurerm_key_vault.ia_key_vault.vault_uri}"
-}
-
 data "azurerm_key_vault_secret" "s2s_secret" {
   name      = "s2s-secret"
   vault_uri = "${data.azurerm_key_vault.ia_key_vault.vault_uri}"
@@ -247,26 +242,6 @@ data "azurerm_key_vault_secret" "s2s_secret" {
 
 data "azurerm_key_vault_secret" "s2s_microservice" {
   name      = "s2s-microservice"
-  vault_uri = "${data.azurerm_key_vault.ia_key_vault.vault_uri}"
-}
-
-data "azurerm_key_vault_secret" "ccd_url" {
-  name      = "ccd-url"
-  vault_uri = "${data.azurerm_key_vault.ia_key_vault.vault_uri}"
-}
-
-data "azurerm_key_vault_secret" "dm_url" {
-  name      = "dm-url"
-  vault_uri = "${data.azurerm_key_vault.ia_key_vault.vault_uri}"
-}
-
-data "azurerm_key_vault_secret" "idam_url" {
-  name      = "idam-url"
-  vault_uri = "${data.azurerm_key_vault.ia_key_vault.vault_uri}"
-}
-
-data "azurerm_key_vault_secret" "s2s_url" {
-  name      = "s2s-url"
   vault_uri = "${data.azurerm_key_vault.ia_key_vault.vault_uri}"
 }
 
@@ -336,14 +311,8 @@ module "ia_case_notifications_api" {
     IA_SYSTEM_PASSWORD   = "${data.azurerm_key_vault_secret.system_password.value}"
     IA_IDAM_CLIENT_ID    = "${data.azurerm_key_vault_secret.idam_client_id.value}"
     IA_IDAM_SECRET       = "${data.azurerm_key_vault_secret.idam_secret.value}"
-    IA_IDAM_REDIRECT_URI = "${data.azurerm_key_vault_secret.idam_redirect_uri.value}"
     IA_S2S_SECRET        = "${data.azurerm_key_vault_secret.s2s_secret.value}"
     IA_S2S_MICROSERVICE  = "${data.azurerm_key_vault_secret.s2s_microservice.value}"
-
-    CCD_URL  = "${data.azurerm_key_vault_secret.ccd_url.value}"
-    DM_URL   = "${data.azurerm_key_vault_secret.dm_url.value}"
-    IDAM_URL = "${data.azurerm_key_vault_secret.idam_url.value}"
-    S2S_URL  = "${data.azurerm_key_vault_secret.s2s_url.value}"
 
     ROOT_LOGGING_LEVEL   = "${var.root_logging_level}"
     LOG_LEVEL_SPRING_WEB = "${var.log_level_spring_web}"
