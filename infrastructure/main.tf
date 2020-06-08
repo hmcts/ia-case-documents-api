609cef8516af0708807d2c5873d92cd27558ba8e
makowalski <marcin.kowalski.it.consulting@gmail.com>
2020-06-08 09:20:09 +0100

    terraform cleanup

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -96,42 +96,4 @@ data "azurerm_lb" "consul_dns" {
   resource_group_name = "${var.consul_dns_resource_group_name}"
 }
 
-module "ia_case_documents_api" {
-  source                          = "git@github.com:hmcts/cnp-module-webapp?ref=master"
-  product                         = "${var.product}-${var.component}"
-  location                        = "${var.location}"
-  env                             = "${var.env}"
-  enable_ase                      = "${var.enable_ase}"
-  ilbIp                           = "${var.ilbIp}"
-  resource_group_name             = "${azurerm_resource_group.rg.name}"
-  subscription                    = "${var.subscription}"
-  capacity                        = "${var.capacity}"
-  instance_size                   = "${var.instance_size}"
-  common_tags                     = "${merge(var.common_tags, map("lastUpdated", "${timestamp()}"))}"
-  appinsights_instrumentation_key = "${var.appinsights_instrumentation_key}"
-  asp_name                        = "${local.app_service_plan}"
-  asp_rg                          = "${local.app_service_plan}"
-
-  app_settings = {
-    LOGBACK_REQUIRE_ALERT_LEVEL = false
-    LOGBACK_REQUIRE_ERROR_CODE  = false
-
-    WEBSITE_DNS_SERVER = "${data.azurerm_lb.consul_dns.private_ip_address}"
-
-    IA_CUSTOMER_SERVICES_EMAIL      = "${data.azurerm_key_vault_secret.ia_customer_services_email.value}"
-    IA_CUSTOMER_SERVICES_TELEPHONE  = "${data.azurerm_key_vault_secret.ia_customer_services_telephone.value}"
-
-    IA_SYSTEM_USERNAME   = "${data.azurerm_key_vault_secret.system_username.value}"
-    IA_SYSTEM_PASSWORD   = "${data.azurerm_key_vault_secret.system_password.value}"
-    IA_IDAM_CLIENT_ID    = "${data.azurerm_key_vault_secret.idam_client_id.value}"
-    IA_IDAM_SECRET       = "${data.azurerm_key_vault_secret.idam_secret.value}"
-    IA_S2S_SECRET        = "${data.azurerm_key_vault_secret.s2s_secret.value}"
-    IA_S2S_MICROSERVICE  = "${data.azurerm_key_vault_secret.s2s_microservice.value}"
-
-    IA_EM_STITCHING_ENABLED  = "${data.azurerm_key_vault_secret.em_stitching_enabled.value}"
-
-    ROOT_LOGGING_LEVEL   = "${var.root_logging_level}"
-    LOG_LEVEL_SPRING_WEB = "${var.log_level_spring_web}"
-    LOG_LEVEL_IA         = "${var.log_level_ia}"
-  }
 }
