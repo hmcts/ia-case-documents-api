c871ba55f1d7a28b0d904f4b5b33a9183d529d67
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2020-07-08 10:46:59 +0100

    Added test to cover switch statement.

--- src/test/java/uk/gov/hmcts/reform/iacasedocumentsapi/infrastructure/HearingDetailsFinderTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacasedocumentsapi/infrastructure/HearingDetailsFinderTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.AsylumCaseDefinition.LIST_CASE_HEARING_CENTRE;
 import static uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.AsylumCaseDefinition.LIST_CASE_HEARING_DATE;
+import static uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.HearingCentre.*;
 
 import java.util.Optional;
 import org.junit.Before;
@@ -93,5 +94,29 @@ public class HearingDetailsFinderTest {
         assertThatThrownBy(() -> hearingDetailsFinder.getHearingCentreAddress(asylumCase))
             .isExactlyInstanceOf(IllegalStateException.class)
             .hasMessage("listCaseHearingCentre is not present");
+    }
+
+    @Test
+    public void should_return_hearing_centres_urls() {
+
+        String birminghamHearingCentreUrl = "https://courttribunalfinder.service.gov.uk/courts/birmingham-immigration-and-asylum-chamber-first-tier-tribunal";
+        String bradfordHearingCentreUrl = "https://courttribunalfinder.service.gov.uk/courts/bradford-tribunal-hearing-centre";
+        String glasgowHearingCentreUrl = "https://courttribunalfinder.service.gov.uk/courts/glasgow-employment-and-immigration-tribunals-eagle-building";
+        String hattonCrossHearingCentreUrl = "https://courttribunalfinder.service.gov.uk/courts/hatton-cross-tribunal-hearing-centre";
+        String taylorHouseHearingCentreUrl = "https://courttribunalfinder.service.gov.uk/courts/taylor-house-tribunal-hearing-centre";
+        String manchesterHearingCentreUrl = "https://courttribunalfinder.service.gov.uk/courts/manchester-tribunal-hearing-centre";
+        String newportHearingCentreUrl = "https://courttribunalfinder.service.gov.uk/courts/newport-south-wales-immigration-and-asylum-tribunal";
+        String nottinghamHearingCentreUrl = "https://courttribunalfinder.service.gov.uk/courts/nottingham-magistrates-court";
+        String northShieldsHearingCentreUrl = "https://courttribunalfinder.service.gov.uk/courts/newcastle-civil-family-courts-and-tribunals-centre";
+
+        assertEquals(birminghamHearingCentreUrl, hearingDetailsFinder.getHearingCentreUrl(BIRMINGHAM));
+        assertEquals(bradfordHearingCentreUrl, hearingDetailsFinder.getHearingCentreUrl(BRADFORD));
+        assertEquals(glasgowHearingCentreUrl, hearingDetailsFinder.getHearingCentreUrl(GLASGOW));
+        assertEquals(hattonCrossHearingCentreUrl, hearingDetailsFinder.getHearingCentreUrl(HATTON_CROSS));
+        assertEquals(taylorHouseHearingCentreUrl, hearingDetailsFinder.getHearingCentreUrl(TAYLOR_HOUSE));
+        assertEquals(manchesterHearingCentreUrl, hearingDetailsFinder.getHearingCentreUrl(MANCHESTER));
+        assertEquals(newportHearingCentreUrl, hearingDetailsFinder.getHearingCentreUrl(NEWPORT));
+        assertEquals(nottinghamHearingCentreUrl, hearingDetailsFinder.getHearingCentreUrl(NOTTINGHAM));
+        assertEquals(northShieldsHearingCentreUrl, hearingDetailsFinder.getHearingCentreUrl(NORTH_SHIELDS));
     }
 }
