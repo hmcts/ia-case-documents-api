65c5014a92aaa8b12d35bfadd719b677b41c7eae
makowalski <marcin.kowalski.it.consulting@gmail.com>
2020-07-15 14:37:34 +0100

    handle errors gracefully within generic handler (#305)

--- src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/handlers/presubmit/NotificationHandler.java
+++ src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/handlers/presubmit/NotificationHandler.java
@@ -3,11 +3,13 @@ package uk.gov.hmcts.reform.iacasenotificationsapi.domain.handlers.presubmit;
 import static java.util.Objects.requireNonNull;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.function.BiPredicate;
 import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.AsylumCase;
 import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.ccd.callback.Callback;
 import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.ccd.callback.PreSubmitCallbackResponse;
 import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.ccd.callback.PreSubmitCallbackStage;
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.handlers.ErrorHandler;
 import uk.gov.hmcts.reform.iacasenotificationsapi.domain.handlers.PreSubmitCallbackHandler;
 import uk.gov.hmcts.reform.iacasenotificationsapi.domain.service.NotificationGenerator;
 
@@ -15,11 +17,23 @@ public class NotificationHandler implements PreSubmitCallbackHandler<AsylumCase>
 
     private final BiPredicate<PreSubmitCallbackStage, Callback<AsylumCase>> canHandleFunction;
     private final List<? extends NotificationGenerator> notificationGenerators;
+    private final Optional<ErrorHandler> errorHandling;
 
     public NotificationHandler(BiPredicate<PreSubmitCallbackStage, Callback<AsylumCase>> canHandleFunction,
-        List<? extends NotificationGenerator> notificationGenerator) {
+                               List<? extends NotificationGenerator> notificationGenerator
+    ) {
         this.canHandleFunction = canHandleFunction;
         this.notificationGenerators = notificationGenerator;
+        this.errorHandling = Optional.empty();
+    }
+
+    public NotificationHandler(BiPredicate<PreSubmitCallbackStage, Callback<AsylumCase>> canHandleFunction,
+                               List<? extends NotificationGenerator> notificationGenerator,
+                               ErrorHandler errorHandling
+    ) {
+        this.canHandleFunction = canHandleFunction;
+        this.notificationGenerators = notificationGenerator;
+        this.errorHandling = Optional.ofNullable(errorHandling);
     }
 
     @Override
@@ -37,8 +51,15 @@ public class NotificationHandler implements PreSubmitCallbackHandler<AsylumCase>
             throw new IllegalStateException("Cannot handle callback");
         }
 
-        notificationGenerators.forEach(notificationGenerator -> notificationGenerator.generate(callback));
-
+        try {
+            notificationGenerators.forEach(notificationGenerator -> notificationGenerator.generate(callback));
+        } catch (Exception e) {
+            if (errorHandling.isPresent()) {
+                errorHandling.get().accept(callback, e);
+            } else {
+                throw e;
+            }
+        }
 
         return new PreSubmitCallbackResponse<>(callback.getCaseDetails().getCaseData());
     }
--- src/test/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/handlers/presubmit/NotificationHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/handlers/presubmit/NotificationHandlerTest.java
@@ -2,9 +2,7 @@ package uk.gov.hmcts.reform.iacasenotificationsapi.domain.handlers.presubmit;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyZeroInteractions;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 import java.util.Collections;
 import java.util.function.BiPredicate;
@@ -18,6 +16,7 @@ import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.ccd.CaseDetail
 import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.ccd.callback.Callback;
 import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.ccd.callback.PreSubmitCallbackResponse;
 import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.ccd.callback.PreSubmitCallbackStage;
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.handlers.ErrorHandler;
 import uk.gov.hmcts.reform.iacasenotificationsapi.domain.service.NotificationGenerator;
 
 
@@ -29,6 +28,7 @@ public class NotificationHandlerTest {
     @Mock AsylumCase asylumCase;
     @Mock NotificationGenerator notificationGenerator;
     @Mock BiPredicate<PreSubmitCallbackStage, Callback<AsylumCase>> canHandle;
+    @Mock ErrorHandler errorHandler;
 
     private PreSubmitCallbackStage callbackStage = PreSubmitCallbackStage.ABOUT_TO_SUBMIT;
     private NotificationHandler notificationHandler;
@@ -81,5 +81,28 @@ public class NotificationHandlerTest {
         assertThatThrownBy(() -> notificationHandler.canHandle(callbackStage, null))
             .isExactlyInstanceOf(NullPointerException.class)
             .hasMessage("callback must not be null");
+    }
+
+    @Test
+    public void should_catch_exception_and_invoke_error_handler() {
+        String message = "exception happened";
+        Throwable exception = new RuntimeException(message);
+        doThrow(exception).when(notificationGenerator).generate(callback);
+        notificationHandler = new NotificationHandler(canHandle, Collections.singletonList(notificationGenerator), errorHandler);
+
+        notificationHandler.handle(callbackStage, callback);
+
+        verify(errorHandler).accept(callback, exception);
+    }
+
+    @Test
+    public void should_re_throw_exception_from_generator() {
+        String message = "exception happened";
+        doThrow(new RuntimeException(message)).when(notificationGenerator).generate(callback);
+        notificationHandler = new NotificationHandler(canHandle, Collections.singletonList(notificationGenerator));
+
+        assertThatThrownBy(() -> notificationHandler.handle(callbackStage, callback))
+            .isExactlyInstanceOf(RuntimeException.class)
+            .hasMessage(message);
     }
 }
