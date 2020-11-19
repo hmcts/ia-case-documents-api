ecb881891336e85128b497fd20b6b8e9e65b2204
Moussa Kayad <mkayad@gmail.com>
2020-11-19 14:29:32 +0000

    RIA-3105-change-due-date (#408)

--- src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/personalisation/respondent/RespondentChangeDirectionDueDatePersonalisation.java
+++ src/main/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/personalisation/respondent/RespondentChangeDirectionDueDatePersonalisation.java
@@ -4,16 +4,16 @@ import static java.util.Objects.requireNonNull;
 import static uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.AsylumCaseDefinition.CURRENT_CASE_STATE_VISIBLE_TO_HOME_OFFICE_ALL;
 
 import com.google.common.collect.ImmutableMap;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.AsylumCase;
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.AsylumCaseDefinition;
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.HearingCentre;
 import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.ccd.State;
 import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.ccd.callback.Callback;
 import uk.gov.hmcts.reform.iacasenotificationsapi.domain.personalisation.EmailNotificationPersonalisation;
+import uk.gov.hmcts.reform.iacasenotificationsapi.infrastructure.AppealService;
 import uk.gov.hmcts.reform.iacasenotificationsapi.infrastructure.CustomerServicesProvider;
 import uk.gov.hmcts.reform.iacasenotificationsapi.infrastructure.EmailAddressFinder;
 import uk.gov.hmcts.reform.iacasenotificationsapi.infrastructure.PersonalisationProvider;
@@ -21,92 +21,100 @@ import uk.gov.hmcts.reform.iacasenotificationsapi.infrastructure.Personalisation
 @Service
 public class RespondentChangeDirectionDueDatePersonalisation implements EmailNotificationPersonalisation {
 
+    public static final String CURRENT_CASE_STATE_VISIBLE_TO_HOME_OFFICE_ALL_FLAG_IS_NOT_PRESENT = "currentCaseStateVisibleToHomeOfficeAll flag is not present";
     private final String respondentChangeDirectionDueDateBeforeListingTemplateId;
     private final String respondentChangeDirectionDueDateAfterListingTemplateId;
     private final String iaExUiFrontendUrl;
     private final PersonalisationProvider personalisationProvider;
-    private final String respondentEmailAddressUntilRespondentReview;
-    private final String respondentEmailAddressAtRespondentReview;
-    private final EmailAddressFinder respondentEmailAddressAfterRespondentReview;
+    private final String apcHomeOfficeEmailAddress;
+    private final String lartHomeOfficeEmailAddress;
     private final CustomerServicesProvider customerServicesProvider;
+    private final AppealService appealService;
+    private final EmailAddressFinder emailAddressFinder;
 
     public RespondentChangeDirectionDueDatePersonalisation(
         @Value("${govnotify.template.changeDirectionDueDate.respondent.afterListing.email}") String respondentChangeDirectionDueDateAfterListingTemplateId,
         @Value("${govnotify.template.changeDirectionDueDate.respondent.beforeListing.email}") String respondentChangeDirectionDueDateBeforeListingTemplateId,
         @Value("${iaExUiFrontendUrl}") String iaExUiFrontendUrl,
         PersonalisationProvider personalisationProvider,
-        @Value("${respondentEmailAddresses.nonStandardDirectionUntilListing}") String respondentEmailAddressUntilRespondentReview,
-        @Value("${respondentEmailAddresses.respondentReviewDirection}") String respondentEmailAddressAtRespondentReview,
-        EmailAddressFinder respondentEmailAddressAfterRespondentReview,
-        CustomerServicesProvider customerServicesProvider
+        @Value("${apcHomeOfficeEmailAddress}") String apcHomeOfficeEmailAddress,
+        @Value("${lartHomeOfficeEmailAddress}") String lartHomeOfficeEmailAddress,
+        CustomerServicesProvider customerServicesProvider,
+        AppealService appealService,
+        EmailAddressFinder emailAddressFinder
     ) {
         requireNonNull(iaExUiFrontendUrl, "iaExUiFrontendUrl must not be null");
         this.respondentChangeDirectionDueDateAfterListingTemplateId = respondentChangeDirectionDueDateAfterListingTemplateId;
         this.respondentChangeDirectionDueDateBeforeListingTemplateId = respondentChangeDirectionDueDateBeforeListingTemplateId;
         this.iaExUiFrontendUrl = iaExUiFrontendUrl;
         this.personalisationProvider = personalisationProvider;
-        this.respondentEmailAddressUntilRespondentReview = respondentEmailAddressUntilRespondentReview;
-        this.respondentEmailAddressAtRespondentReview = respondentEmailAddressAtRespondentReview;
-        this.respondentEmailAddressAfterRespondentReview = respondentEmailAddressAfterRespondentReview;
+        this.apcHomeOfficeEmailAddress = apcHomeOfficeEmailAddress;
+        this.lartHomeOfficeEmailAddress = lartHomeOfficeEmailAddress;
         this.customerServicesProvider = customerServicesProvider;
+        this.appealService = appealService;
+        this.emailAddressFinder = emailAddressFinder;
     }
 
     @Override
     public String getTemplateId(AsylumCase asylumCase) {
-
-        return asylumCase.read(CURRENT_CASE_STATE_VISIBLE_TO_HOME_OFFICE_ALL, State.class)
-            .map(s -> {
-                if (Arrays.asList(
-                    State.APPEAL_SUBMITTED,
-                    State.PENDING_PAYMENT,
-                    State.APPEAL_SUBMITTED_OUT_OF_TIME,
-                    State.AWAITING_RESPONDENT_EVIDENCE,
-                    State.CASE_BUILDING,
-                    State.CASE_UNDER_REVIEW,
-                    State.RESPONDENT_REVIEW,
-                    State.SUBMIT_HEARING_REQUIREMENTS
-                ).contains(s)) {
-                    return respondentChangeDirectionDueDateBeforeListingTemplateId;
-                }
-
-                return respondentChangeDirectionDueDateAfterListingTemplateId;
-            })
-            .orElseThrow(() -> new IllegalStateException("currentCaseStateVisibleToHomeOfficeAll flag is not present"));
+        return appealService.isAppealListed(asylumCase) ? respondentChangeDirectionDueDateAfterListingTemplateId : respondentChangeDirectionDueDateBeforeListingTemplateId;
     }
 
     @Override
     public Set<String> getRecipientsList(AsylumCase asylumCase) {
-        return Collections.singleton(getRespondentEmailAddress(asylumCase));
+
+        return asylumCase.read(CURRENT_CASE_STATE_VISIBLE_TO_HOME_OFFICE_ALL, State.class)
+            .map(currentState -> {
+                if (Arrays.asList(
+                    State.APPEAL_SUBMITTED,
+                    State.PENDING_PAYMENT,
+                    State.AWAITING_RESPONDENT_EVIDENCE
+                ).contains(currentState)) {
+                    return Collections.singleton(apcHomeOfficeEmailAddress);
+                } else if (Arrays.asList(
+                    State.CASE_BUILDING,
+                    State.CASE_UNDER_REVIEW,
+                    State.RESPONDENT_REVIEW
+                ).contains(currentState)) {
+                    return Collections.singleton(lartHomeOfficeEmailAddress);
+                } else if (Arrays.asList(
+                    State.FTPA_SUBMITTED,
+                    State.FTPA_DECIDED).contains(currentState)) {
+                    return Collections.singleton(emailAddressFinder.getListCaseFtpaHomeOfficeEmailAddress(asylumCase));
+                } else if (Arrays.asList(
+                    State.LISTING,
+                    State.SUBMIT_HEARING_REQUIREMENTS,
+                    State.ENDED,
+                    State.APPEAL_TAKEN_OFFLINE).contains(currentState)
+                           && !appealService.isAppealListed(asylumCase)) {
+                    return  Collections.singleton(emailAddressFinder.getHomeOfficeEmailAddress(asylumCase));
+                } else if (Arrays.asList(
+                    State.PREPARE_FOR_HEARING,
+                    State.FINAL_BUNDLING,
+                    State.PRE_HEARING,
+                    State.DECISION,
+                    State.ADJOURNED,
+                    State.DECIDED,
+                    State.ENDED,
+                    State.APPEAL_TAKEN_OFFLINE
+                ).contains(currentState) && appealService.isAppealListed(asylumCase)) {
+                    final Optional<HearingCentre> maybeCaseIsListed = asylumCase
+                        .read(AsylumCaseDefinition.LIST_CASE_HEARING_CENTRE, HearingCentre.class);
+
+                    if (maybeCaseIsListed.isPresent()) {
+                        return Collections.singleton(emailAddressFinder.getListCaseHomeOfficeEmailAddress(asylumCase));
+                    } else {
+                        return  Collections.singleton(emailAddressFinder.getHomeOfficeEmailAddress(asylumCase));
+                    }
+                }
+                throw new IllegalStateException(CURRENT_CASE_STATE_VISIBLE_TO_HOME_OFFICE_ALL_FLAG_IS_NOT_PRESENT);
+            })
+        .orElseThrow(() -> new IllegalStateException(CURRENT_CASE_STATE_VISIBLE_TO_HOME_OFFICE_ALL_FLAG_IS_NOT_PRESENT));
     }
 
     @Override
     public String getReferenceId(Long caseId) {
         return caseId + "_RESPONDENT_CHANGE_DIRECTION_DUE_DATE";
-    }
-
-    private String getRespondentEmailAddress(AsylumCase asylumCase) {
-
-        return asylumCase.read(CURRENT_CASE_STATE_VISIBLE_TO_HOME_OFFICE_ALL, State.class)
-            .map(s -> {
-                if (Arrays.asList(
-                    State.APPEAL_SUBMITTED,
-                    State.PENDING_PAYMENT,
-                    State.APPEAL_SUBMITTED_OUT_OF_TIME,
-                    State.AWAITING_RESPONDENT_EVIDENCE,
-                    State.CASE_BUILDING,
-                    State.CASE_UNDER_REVIEW
-                ).contains(s)) {
-                    return respondentEmailAddressUntilRespondentReview;
-                } else if (Arrays.asList(
-                    State.RESPONDENT_REVIEW,
-                    State.SUBMIT_HEARING_REQUIREMENTS
-                ).contains(s)) {
-                    return respondentEmailAddressAtRespondentReview;
-                }
-
-                return respondentEmailAddressAfterRespondentReview.getListCaseHomeOfficeEmailAddress(asylumCase);
-            })
-            .orElseThrow(() -> new IllegalStateException("currentCaseStateVisibleToHomeOfficeAll flag is not present"));
     }
 
     @Override
--- src/test/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/personalisation/respondent/RespondentChangeDirectionDueDatePersonalisationTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacasenotificationsapi/domain/personalisation/respondent/RespondentChangeDirectionDueDatePersonalisationTest.java
@@ -1,14 +1,16 @@
 package uk.gov.hmcts.reform.iacasenotificationsapi.domain.personalisation.respondent;
 
+import static com.google.common.collect.Lists.newArrayList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.AsylumCaseDefinition.HEARING_CENTRE;
+import static uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.AsylumCaseDefinition.LIST_CASE_HEARING_CENTRE;
 
 import com.google.common.collect.ImmutableMap;
-import java.util.Collections;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -18,8 +20,10 @@ import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
 import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.AsylumCase;
 import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.AsylumCaseDefinition;
+import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.HearingCentre;
 import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.ccd.State;
 import uk.gov.hmcts.reform.iacasenotificationsapi.domain.entities.ccd.callback.Callback;
+import uk.gov.hmcts.reform.iacasenotificationsapi.infrastructure.AppealService;
 import uk.gov.hmcts.reform.iacasenotificationsapi.infrastructure.CustomerServicesProvider;
 import uk.gov.hmcts.reform.iacasenotificationsapi.infrastructure.EmailAddressFinder;
 import uk.gov.hmcts.reform.iacasenotificationsapi.infrastructure.PersonalisationProvider;
@@ -39,41 +43,56 @@ public class RespondentChangeDirectionDueDatePersonalisationTest {
     PersonalisationProvider personalisationProvider;
     @Mock
     CustomerServicesProvider customerServicesProvider;
+    @Mock
+    AppealService appealService;
 
-    private Long caseId = 12345L;
-    private String afterListingTemplateId = "afterListingTemplateId";
-    private String beforeListingTemplateId = "beforeListingTemplateId";
-    private String iaExUiFrontendUrl = "http://localhost";
-    private String homeOfficeApcEmailAddress = "homeOfficeAPC@example.com";
-    private String homeOfficeLartEmailAddress = "homeOfficeLART@example.com";
-    private String homeOfficeBhamEmailAddress = "ho-birmingham@example.com";
-    private String hmctsReference = "hmctsReference";
-    private String ariaListingReference = "someAriaListingReference";
-    private String homeOfficeReference = "homeOfficeReference";
-    private String appellantGivenNames = "someAppellantGivenNames";
-    private String appellantFamilyName = "someAppellantFamilyName";
-    private String customerServicesTelephone = "555 555 555";
-    private String customerServicesEmail = "cust.services@example.com";
+    private final Long caseId = 12345L;
+    private final String afterListingTemplateId = "afterListingTemplateId";
+    private final String beforeListingTemplateId = "beforeListingTemplateId";
+    private final String iaExUiFrontendUrl = "http://localhost";
+    private final String apcHomeOfficeEmailAddress = "homeoffice-apc@example.com";
+    private final String lartHomeOfficeEmailAddress = "homeoffice-respondent@example.com";
+    private final String homeOfficeHearingCentreEmail = "hc-taylorhouse@example.com";
+    private final String homeOfficeEmail = "ho-taylorhouse@example.com";
+    private final String homeOfficeFtpaEmailAddress = "ho-ftpa-taylorhouse@example.com";
+
+
+    private final String hmctsReference = "hmctsReference";
+    private final String ariaListingReference = "someAriaListingReference";
+    private final String homeOfficeReference = "homeOfficeReference";
+    private final String appellantGivenNames = "someAppellantGivenNames";
+    private final String appellantFamilyName = "someAppellantFamilyName";
+    private final String customerServicesTelephone = "555 555 555";
+    private final String customerServicesEmail = "cust.services@example.com";
+
 
     private RespondentChangeDirectionDueDatePersonalisation respondentChangeDirectionDueDatePersonalisation;
 
     @BeforeEach
     public void setUp() {
 
+        when((emailAddressFinder.getListCaseHomeOfficeEmailAddress(asylumCase)))
+            .thenReturn(homeOfficeHearingCentreEmail);
+
+        when(emailAddressFinder.getListCaseFtpaHomeOfficeEmailAddress(asylumCase)).thenReturn(homeOfficeFtpaEmailAddress);
+
+        when((emailAddressFinder.getHomeOfficeEmailAddress(asylumCase))).thenReturn(homeOfficeEmail);
+
         respondentChangeDirectionDueDatePersonalisation = new RespondentChangeDirectionDueDatePersonalisation(
             afterListingTemplateId,
             beforeListingTemplateId,
             iaExUiFrontendUrl,
             personalisationProvider,
-            homeOfficeApcEmailAddress,
-            homeOfficeLartEmailAddress,
-            emailAddressFinder,
-            customerServicesProvider
+            apcHomeOfficeEmailAddress,
+            lartHomeOfficeEmailAddress,
+            customerServicesProvider,
+            appealService,
+            emailAddressFinder
         );
     }
 
     @Test
-    public void should_return_the_given_before_listing_template_id() {
+    void should_return_the_given_before_listing_template_id() {
         when(asylumCase.read(AsylumCaseDefinition.CURRENT_CASE_STATE_VISIBLE_TO_HOME_OFFICE_ALL, State.class))
             .thenReturn(Optional.of(State.CASE_BUILDING));
 
@@ -82,51 +101,95 @@ public class RespondentChangeDirectionDueDatePersonalisationTest {
     }
 
     @Test
-    public void should_return_the_given_after_listing_template_id() {
-        when(asylumCase.read(AsylumCaseDefinition.CURRENT_CASE_STATE_VISIBLE_TO_HOME_OFFICE_ALL, State.class))
-            .thenReturn(Optional.of(State.FINAL_BUNDLING));
-
+    void should_return_the_given_after_listing_template_id() {
+        when(appealService.isAppealListed(asylumCase)).thenReturn(true);
         assertEquals(afterListingTemplateId, respondentChangeDirectionDueDatePersonalisation.getTemplateId(asylumCase));
     }
 
     @Test
-    public void should_throw_exception_if_current_visible_state_to_home_office_all_is_not_present() {
+    void should_return_correct_email_address_for_home_office() {
 
-        assertThatThrownBy(() -> respondentChangeDirectionDueDatePersonalisation.getTemplateId(asylumCase))
-            .isExactlyInstanceOf(IllegalStateException.class)
-            .hasMessage("currentCaseStateVisibleToHomeOfficeAll flag is not present");
+        List<State> apcEmail = newArrayList(
+            State.APPEAL_SUBMITTED,
+            State.PENDING_PAYMENT,
+            State.AWAITING_RESPONDENT_EVIDENCE
+
+        );
+
+
+        List<State> lartEmail = newArrayList(
+            State.CASE_BUILDING,
+            State.CASE_UNDER_REVIEW,
+            State.RESPONDENT_REVIEW
+        );
+
+        List<State> ftpaEmail = newArrayList(
+            State.FTPA_SUBMITTED,
+            State.FTPA_DECIDED
+        );
+
+        List<State> pouNoListedEmail = newArrayList(
+            State.LISTING,
+            State.SUBMIT_HEARING_REQUIREMENTS,
+            State.ENDED,
+            State.APPEAL_TAKEN_OFFLINE
+        );
+
+        List<State> poulistedEmail = newArrayList(
+            State.PREPARE_FOR_HEARING,
+            State.FINAL_BUNDLING,
+            State.PRE_HEARING,
+            State.DECISION,
+            State.ADJOURNED,
+            State.DECIDED,
+            State.ENDED,
+            State.APPEAL_TAKEN_OFFLINE
+        );
+
+        Map<String, List<State>> states = new HashMap<>();
+
+        states.put(apcHomeOfficeEmailAddress, apcEmail);
+        states.put(lartHomeOfficeEmailAddress, lartEmail);
+        states.put(homeOfficeFtpaEmailAddress, ftpaEmail);
+        states.put(homeOfficeEmail, pouNoListedEmail);
+        states.put(homeOfficeHearingCentreEmail, poulistedEmail);
+
+
+        Set<String> emailAddresses = states.keySet();
+
+        for (String emailAddress : emailAddresses) {
+            List<State> statesList = states.get(emailAddress);
+            for (State state : statesList) {
+                when(asylumCase.read(AsylumCaseDefinition.CURRENT_CASE_STATE_VISIBLE_TO_HOME_OFFICE_ALL, State.class))
+                    .thenReturn(Optional.of(state));
+
+                if (emailAddress != null && emailAddress.equals(homeOfficeHearingCentreEmail)) {
+                    // test the same state when the case is listed
+                    when(appealService.isAppealListed(asylumCase)).thenReturn(true);
+                    when(asylumCase.read(HEARING_CENTRE)).thenReturn(Optional.of(Optional.empty()));
+                    when(asylumCase.read(LIST_CASE_HEARING_CENTRE, HearingCentre.class))
+                        .thenReturn(Optional.of(HearingCentre.TAYLOR_HOUSE));
+
+                    assertTrue(respondentChangeDirectionDueDatePersonalisation.getRecipientsList(asylumCase)
+                        .contains(homeOfficeHearingCentreEmail));
+                } else if (emailAddress != null && emailAddress.equals(homeOfficeEmail)) {
+                    //case not listed yet
+                    when(appealService.isAppealListed(asylumCase)).thenReturn(false);
+                    when(asylumCase.read(HEARING_CENTRE)).thenReturn(Optional.of(HearingCentre.TAYLOR_HOUSE));
+                    when(asylumCase.read(LIST_CASE_HEARING_CENTRE, HearingCentre.class)).thenReturn(Optional.empty());
+
+                    assertTrue(respondentChangeDirectionDueDatePersonalisation.getRecipientsList(asylumCase)
+                        .contains(homeOfficeEmail));
+                } else {
+                    assertTrue(respondentChangeDirectionDueDatePersonalisation.getRecipientsList(asylumCase)
+                        .contains(emailAddress));
+                }
+            }
+        }
     }
 
     @Test
-    public void should_return_the_ho_apc_email_address_until_respondent_review() {
-        when(asylumCase.read(AsylumCaseDefinition.CURRENT_CASE_STATE_VISIBLE_TO_HOME_OFFICE_ALL, State.class))
-            .thenReturn(Optional.of(State.CASE_BUILDING));
-
-        assertEquals(Collections.singleton(homeOfficeApcEmailAddress),
-            respondentChangeDirectionDueDatePersonalisation.getRecipientsList(asylumCase));
-    }
-
-    @Test
-    public void should_return_the_ho_lart_email_address_at_respondent_review() {
-        when(asylumCase.read(AsylumCaseDefinition.CURRENT_CASE_STATE_VISIBLE_TO_HOME_OFFICE_ALL, State.class))
-            .thenReturn(Optional.of(State.RESPONDENT_REVIEW));
-
-        assertEquals(Collections.singleton(homeOfficeLartEmailAddress),
-            respondentChangeDirectionDueDatePersonalisation.getRecipientsList(asylumCase));
-    }
-
-    @Test
-    public void should_return_the_ho_hearing_centre_email_address_after_respondent_review() {
-        when(emailAddressFinder.getListCaseHomeOfficeEmailAddress(asylumCase)).thenReturn(homeOfficeBhamEmailAddress);
-        when(asylumCase.read(AsylumCaseDefinition.CURRENT_CASE_STATE_VISIBLE_TO_HOME_OFFICE_ALL, State.class))
-            .thenReturn(Optional.of(State.FINAL_BUNDLING));
-
-        assertEquals(Collections.singleton(homeOfficeBhamEmailAddress),
-            respondentChangeDirectionDueDatePersonalisation.getRecipientsList(asylumCase));
-    }
-
-    @Test
-    public void should_throw_exception_when_home_office_is_missing_in_the_case_data() {
+    void should_throw_exception_when_home_office_is_missing_in_the_case_data() {
         when(asylumCase.read(AsylumCaseDefinition.CURRENT_CASE_STATE_VISIBLE_TO_HOME_OFFICE_ALL, State.class))
             .thenReturn(Optional.empty());
 
@@ -136,13 +199,13 @@ public class RespondentChangeDirectionDueDatePersonalisationTest {
     }
 
     @Test
-    public void should_return_given_reference_id() {
+    void should_return_given_reference_id() {
         assertEquals(caseId + "_RESPONDENT_CHANGE_DIRECTION_DUE_DATE",
             respondentChangeDirectionDueDatePersonalisation.getReferenceId(caseId));
     }
 
     @Test
-    public void should_return_given_personalisation_when_all_information_given() {
+    void should_return_given_personalisation_when_all_information_given() {
         when(personalisationProvider.getPersonalisation(callback)).thenReturn(getPersonalisation());
 
         Map<String, String> personalisation =
@@ -152,7 +215,7 @@ public class RespondentChangeDirectionDueDatePersonalisationTest {
     }
 
     @Test
-    public void should_throw_exception_on_personalisation_when_case_is_null() {
+    void should_throw_exception_on_personalisation_when_case_is_null() {
         assertThatThrownBy(
             () -> respondentChangeDirectionDueDatePersonalisation.getPersonalisation((Callback<AsylumCase>) null))
             .isExactlyInstanceOf(NullPointerException.class)
