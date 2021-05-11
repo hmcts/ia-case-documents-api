c64ef01612d028e27eea330030069e07c3e412f4
Moussa Kayad <mkayad@gmail.com>
2021-05-11 12:15:55 +0100

    RIA-4330 upgrading spring to 2.4.5

--- src/main/java/uk/gov/hmcts/reform/iacasedocumentsapi/Application.java
+++ src/main/java/uk/gov/hmcts/reform/iacasedocumentsapi/Application.java
@@ -2,11 +2,11 @@ package uk.gov.hmcts.reform.iacasedocumentsapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableCircuitBreaker
+@EnableHystrix
 @EnableFeignClients(basePackages =
     {
     "uk.gov.hmcts.reform.auth",
