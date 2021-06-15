b0731346a5d93ecb7a3d74f5487d856b98c77a9e
makowalski <marcin.kowalski.it.consulting@gmail.com>
2021-06-15 14:05:08 +0100

    RIA-4496 new keywords (#179)

--- src/integrationTest/java/uk/gov/hmcts/reform/iacasepaymentsapi/testutils/WithFeeStub.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/iacasepaymentsapi/testutils/WithFeeStub.java
@@ -13,7 +13,10 @@ public interface WithFeeStub {
     default void addFeesRegisterStub(WireMockServer server) {
 
         String feesHearingRegisterUrl = "/fees/fees-register/fees/lookup?channel=default&event=issue"
-            + "&jurisdiction1=tribunal&jurisdiction2=immigration%20and%20asylum%20chamber&keyword=ABC&service=other";
+                                        + "&jurisdiction1=tribunal"
+                                        + "&jurisdiction2=immigration%20and%20asylum%20chamber"
+                                        + "&keyword=HearingOral"
+                                        + "&service=other";
 
         server.addStubMapping(
             new StubMapping(
@@ -23,15 +26,18 @@ public interface WithFeeStub {
                     .withStatus(200)
                     .withHeader("Content-Type", "application/json")
                     .withBody("{\"code\": \"FEE0238\",\n"
-                                  + "  \"description\": \"Appeal determined with a hearing\",\n"
-                                  + "  \"fee_amount\": 140.00,\n"
-                                  + "  \"version\": 2}")
+                              + "  \"description\": \"Appeal determined with a hearing\",\n"
+                              + "  \"fee_amount\": 140.00,\n"
+                              + "  \"version\": 2}")
                     .build()
             )
         );
 
         String feesWithoutHearingRegisterUrl = "/fees/fees-register/fees/lookup?channel=default&event=issue"
-            + "&jurisdiction1=tribunal&jurisdiction2=immigration%20and%20asylum%20chamber&keyword=DEF&service=other";
+                                               + "&jurisdiction1=tribunal"
+                                               + "&jurisdiction2=immigration%20and%20asylum%20chamber"
+                                               + "&keyword=HearingPaper"
+                                               + "&service=other";
 
         server.addStubMapping(
             new StubMapping(
@@ -41,9 +47,9 @@ public interface WithFeeStub {
                     .withStatus(200)
                     .withHeader("Content-Type", "application/json")
                     .withBody("{\"code\": \"FEE0456\",\n"
-                                  + " \"description\": \"Appeal determined without a hearing\",\n"
-                                  + " \"fee_amount\": 80.00,\n"
-                                  + "  \"version\": 2}")
+                              + " \"description\": \"Appeal determined without a hearing\",\n"
+                              + " \"fee_amount\": 80.00,\n"
+                              + "  \"version\": 2}")
                     .build()
             )
         );
