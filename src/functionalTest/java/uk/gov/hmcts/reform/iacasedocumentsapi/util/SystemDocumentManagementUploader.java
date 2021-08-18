0a181ff61ab27d22e5ce2a023c9d7624226573c2
makowalski <marcin.kowalski.it.consulting@gmail.com>
2021-08-18 18:13:52 +0100

    SystemDocumentManagementUploader proof of concept

--- src/functionalTest/java/uk/gov/hmcts/reform/iacasedocumentsapi/util/SystemDocumentManagementUploader.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/iacasedocumentsapi/util/SystemDocumentManagementUploader.java
@@ -3,13 +3,19 @@ package uk.gov.hmcts.reform.iacasedocumentsapi.util;
 import com.google.common.io.ByteStreams;
 import java.io.IOException;
 import java.util.Collections;
+import java.util.UUID;
+
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import uk.gov.hmcts.reform.ccd.document.am.feign.CaseDocumentClient;
+import uk.gov.hmcts.reform.ccd.document.am.model.CaseDocumentsMetadata;
+import uk.gov.hmcts.reform.ccd.document.am.model.DocumentHashToken;
 import uk.gov.hmcts.reform.ccd.document.am.model.UploadResponse;
 import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.ccd.field.Document;
 import uk.gov.hmcts.reform.iacasedocumentsapi.infrastructure.clients.utils.InMemoryMultipartFile;
+
+import static com.google.common.collect.Lists.newArrayList;
 
 @Service
 public class SystemDocumentManagementUploader {
@@ -31,12 +37,12 @@ public class SystemDocumentManagementUploader {
     ) {
         final String serviceAuthorizationToken =
             authorizationHeadersProvider
-                .getLegalRepresentativeAuthorization()
+                .getCaseOfficerAuthorization()
                 .getValue("ServiceAuthorization");
 
         final String accessToken =
             authorizationHeadersProvider
-                .getLegalRepresentativeAuthorization()
+                .getCaseOfficerAuthorization()
                 .getValue("Authorization");
 
         final String userId = "1";
@@ -50,6 +56,7 @@ public class SystemDocumentManagementUploader {
                 ByteStreams.toByteArray(resource.getInputStream())
             );
 
+            // 1629280295439399
             UploadResponse uploadResponse =
                     caseDocumentClient
                             .uploadDocuments(
@@ -64,6 +71,20 @@ public class SystemDocumentManagementUploader {
                 uploadResponse
                     .getDocuments()
                     .get(0);
+
+            caseDocumentClient
+                .patchDocument(
+                    accessToken,
+                    serviceAuthorizationToken,
+                    CaseDocumentsMetadata.builder()
+                        .caseId("1629280295439399")
+                        .caseTypeId("Asylum")
+                        .jurisdictionId("IA")
+                        .documentHashTokens(newArrayList(
+                            DocumentHashToken.builder().hashToken(uploadedDocument.hashToken).id(UUID.randomUUID().toString()).build())
+                        )
+                        .build()
+                );
 
             return new Document(
                 uploadedDocument
