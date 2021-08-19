eb374f45ec07ce76e771838bbf8dfbb26d734c3a
jkumar <jagadeesh.kumar@hmcts.net>
2021-08-19 15:39:18 +0100

    RIA-4540: Added uniqueId and directionType to Direction class - refactor to fix sonar issues

--- src/test/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/entities/DirectionTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/entities/DirectionTest.java
@@ -74,43 +74,44 @@ class DirectionTest {
     void should_not_allow_null_arguments() {
 
         String uniqueId = UUID.randomUUID().toString();
+        List<IdValue<ClarifyingQuestion>> clarifyingQuestions = Collections.emptyList();
         assertThatThrownBy(() -> new Direction(null, parties, dateDue, dateSent, tag, previousDates,
-                Collections.emptyList(),
+                clarifyingQuestions,
                 uniqueId,
                 "someDirectionType"
         ))
                 .isExactlyInstanceOf(NullPointerException.class);
 
         assertThatThrownBy(() -> new Direction(explanation, null, dateDue, dateSent, tag, previousDates,
-                Collections.emptyList(),
+                clarifyingQuestions,
                 uniqueId,
                 "someDirectionType"
         ))
                 .isExactlyInstanceOf(NullPointerException.class);
 
         assertThatThrownBy(() -> new Direction(explanation, parties, null, dateSent, tag, previousDates,
-                Collections.emptyList(),
+                clarifyingQuestions,
                 uniqueId,
                 "someDirectionType"
         ))
                 .isExactlyInstanceOf(NullPointerException.class);
 
         assertThatThrownBy(() -> new Direction(explanation, parties, dateDue, null, tag, previousDates,
-                Collections.emptyList(),
+                clarifyingQuestions,
                 uniqueId,
                 "someDirectionType"
         ))
                 .isExactlyInstanceOf(NullPointerException.class);
 
         assertThatThrownBy(() -> new Direction(explanation, parties, dateDue, dateSent, null, previousDates,
-                Collections.emptyList(),
+                clarifyingQuestions,
                 uniqueId,
                 "someDirectionType"
         ))
                 .isExactlyInstanceOf(NullPointerException.class);
 
         assertThatThrownBy(() -> new Direction(explanation, parties, dateDue, dateSent, tag, null,
-                Collections.emptyList(),
+                clarifyingQuestions,
                 uniqueId,
                 "someDirectionType"
         ))
