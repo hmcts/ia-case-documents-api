a389edfc326a5e41fc9f86f4769ebf2358e1b135
indu2019 <indu.chaturvedi@hmcts.net>
2022-05-16 16:32:46 +0100

    RIA-5679-InduUpdate created a new branch so that i can run this PR

--- src/test/java/uk/gov/hmcts/reform/iacasedocumentsapi/infrastructure/security/idam/IdamAuthoritiesConverterTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacasedocumentsapi/infrastructure/security/idam/IdamAuthoritiesConverterTest.java
@@ -44,14 +44,11 @@ public class IdamAuthoritiesConverterTest {
         when(jwt.getClaim(TOKEN_NAME)).thenReturn(ACCESS_TOKEN);
         when(jwt.getTokenValue()).thenReturn(tokenValue);
 
-        assertTrue(jwt.hasClaim(TOKEN_NAME));
-        assertEquals(jwt.getClaim(TOKEN_NAME).toString(), "access_token");
-
         when(userInfo.getRoles()).thenReturn(Lists.newArrayList("caseworker-ia", "caseworker-ia-caseofficer"));
         when(idamApi.userInfo("Bearer " + tokenValue)).thenReturn(userInfo);
 
         idamAuthoritiesConverter = new IdamAuthoritiesConverter(idamApi);
-
+        //Indu commented it
         List<GrantedAuthority> expectedGrantedAuthorities = Lists.newArrayList(
             new SimpleGrantedAuthority("caseworker-ia"),
             new SimpleGrantedAuthority("caseworker-ia-caseofficer")
@@ -84,9 +81,6 @@ public class IdamAuthoritiesConverterTest {
         when(jwt.getClaim(TOKEN_NAME)).thenReturn(ACCESS_TOKEN);
         when(jwt.getTokenValue()).thenReturn(tokenValue);
 
-        assertTrue(jwt.hasClaim(TOKEN_NAME));
-        assertEquals(jwt.getClaim(TOKEN_NAME).toString(), "access_token");
-
         assertEquals(Collections.emptyList(), idamAuthoritiesConverter.convert(jwt));
     }
 
@@ -98,9 +92,6 @@ public class IdamAuthoritiesConverterTest {
         when(jwt.hasClaim(TOKEN_NAME)).thenReturn(true);
         when(jwt.getClaim(TOKEN_NAME)).thenReturn(ACCESS_TOKEN);
         when(jwt.getTokenValue()).thenReturn(tokenValue);
-
-        assertTrue(jwt.hasClaim(TOKEN_NAME));
-        assertEquals(jwt.getClaim(TOKEN_NAME).toString(), "access_token");
 
         idamAuthoritiesConverter = new IdamAuthoritiesConverter(idamApi);
 
