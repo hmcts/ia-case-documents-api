58002c98d9135f87570072b1c88bd5fef9e8bc55
sajjadh-v1 <106668694+sajjadh-v1@users.noreply.github.com>
2022-09-16 12:24:33 +0100

    Address false p[ositives coming from new Bails APIs to fix Sec Scan (#703)
    Co-authored-by: SajjadH <hassan.sajjad@hmcts.net>

--- security.sh
+++ security.sh
@@ -6,7 +6,7 @@ curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --ou
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 zap-cli --zap-url http://0.0.0.0 -p 1001 report -o /zap/api-report.html -f html
-zap-cli --zap-url http://0.0.0.0 -p 1001 alerts -l Medium --exit-code False
+zap-cli --zap-url http://0.0.0.0 -p 1001 alerts -l Informational --exit-code False
 mkdir -p functional-output/
 cp /zap/api-report.html functional-output/
 cp *.* functional-output/
