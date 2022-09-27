519eea8df452a7b3b369da9e113d4d26476948db
Neha Aggarwal <neha.aggarwal@version1.com>
2022-09-27 23:07:09 +0100

    RIA-6264: Added missing reporting conditions in unsigned decision document

--- src/test/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/entities/BailCaseFieldDefinitionTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/entities/BailCaseFieldDefinitionTest.java
@@ -31,6 +31,6 @@ public class BailCaseFieldDefinitionTest {
 
     @Test
     public void should_fail_if_new_fields_added_in_class() {
-        assertEquals(141, values().length);
+        assertEquals(142, values().length);
     }
 }
