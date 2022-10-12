749c3085ec76979b99da3fb2fe7d878206f74dc8
Neha Aggarwal <neha.aggarwal@version1.com>
2022-10-12 16:02:55 +0100

    Fix unit test failures

--- src/test/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/entities/BailCaseFieldDefinitionTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/entities/BailCaseFieldDefinitionTest.java
@@ -32,6 +32,6 @@ public class BailCaseFieldDefinitionTest {
 
     @Test
     public void should_fail_if_new_fields_added_in_class() {
-        assertEquals(144, values().length);
+        assertEquals(145, values().length);
     }
 }
