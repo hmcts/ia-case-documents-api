c6e408d9d588a44b52a0411ed59cf39192e9d812
rajeshthuraiyur <48379561+rajeshthuraiyur@users.noreply.github.com>
2022-11-10 15:19:18 +0000

    Updated to synch with master
    Updated to synch with master

--- src/main/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/handlers/presubmit/bail/BailDecisionUnsignedGrantedCreator.java
+++ src/main/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/handlers/presubmit/bail/BailDecisionUnsignedGrantedCreator.java
@@ -66,8 +66,7 @@ public class BailDecisionUnsignedGrantedCreator implements PreSubmitCallbackHand
 
         bailDocumentHandler.addWithMetadata(
             bailCase,
-            bailDocument,
-            TRIBUNAL_DOCUMENTS_WITH_METADATA,
+            bailDocument,            
             UNSIGNED_DECISION_DOCUMENTS_WITH_METADATA,
             DocumentTag.BAIL_DECISION_UNSIGNED
         );
