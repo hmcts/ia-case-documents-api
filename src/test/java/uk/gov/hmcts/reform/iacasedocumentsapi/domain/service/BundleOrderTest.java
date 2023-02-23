86607c2a0c8b60c207c6c2289962467a05343f6a
Dbarnes-v1 <daniel.barnes@hmcts.net>
2023-02-23 15:28:09 +0000

    RIA-6702 - Updating unit test

--- src/test/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/service/BundleOrderTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/service/BundleOrderTest.java
@@ -37,7 +37,7 @@ public class BundleOrderTest {
             .map(DocumentWithMetadata::getTag)
             .collect(Collectors.toList());
 
-        assertEquals(32, sortedTags.size());
+        assertEquals(33, sortedTags.size());
 
         List<DocumentTag> documentTagList = Arrays.asList(
             DocumentTag.CASE_SUMMARY,
