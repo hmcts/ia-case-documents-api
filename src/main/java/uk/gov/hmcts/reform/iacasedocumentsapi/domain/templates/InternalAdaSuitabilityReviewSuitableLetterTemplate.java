7b07e219cc1437ab704b32bf9d1418e55a654a4c
FarukH <hussain.faruk@hmcts.net>
2023-06-30 15:34:52 +0100

    RIA-7174 * Removed common method and put it back into the template as a private method.

--- src/main/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/templates/InternalAdaSuitabilityReviewSuitableLetterTemplate.java
+++ src/main/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/templates/InternalAdaSuitabilityReviewSuitableLetterTemplate.java
@@ -2,9 +2,11 @@ package uk.gov.hmcts.reform.iacasedocumentsapi.domain.templates;
 
 import static uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.AsylumCaseDefinition.*;
 import static uk.gov.hmcts.reform.iacasedocumentsapi.domain.utils.DateUtils.formatDateForNotificationAttachmentDocument;
-import static uk.gov.hmcts.reform.iacasedocumentsapi.domain.utils.DateUtils.formatDateTimeForNotificationAttachmentDocument;
 
+import com.google.common.base.Strings;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,6 +20,8 @@ public class InternalAdaSuitabilityReviewSuitableLetterTemplate implements Docum
 
     private final String templateName;
     private final CustomerServicesProvider customerServicesProvider;
+    private static final DateTimeFormatter DOCUMENT_DATE_FORMAT = DateTimeFormatter.ofPattern("ddMMyyyy");
+
 
     public InternalAdaSuitabilityReviewSuitableLetterTemplate(
         @Value("${adaInternalSuitabilityReviewSuitableDocument.templateName}") String templateName,
@@ -48,7 +52,7 @@ public class InternalAdaSuitabilityReviewSuitableLetterTemplate implements Docum
         fieldValues.put("appellantGivenNames", asylumCase.read(APPELLANT_GIVEN_NAMES, String.class).orElse(""));
         fieldValues.put("appellantFamilyName", asylumCase.read(APPELLANT_FAMILY_NAME, String.class).orElse(""));
 
-        fieldValues.put("hearingDate", formatDateTimeForNotificationAttachmentDocument(asylumCase.read(LIST_CASE_HEARING_DATE, String.class).orElse("")));
+        fieldValues.put("hearingDate", formatDateForRendering(asylumCase.read(LIST_CASE_HEARING_DATE, String.class).orElse("")));
         fieldValues.put("hearingType", asylumCase.read(REMOTE_VIDEO_CALL_TRIBUNAL_RESPONSE, String.class).orElse(null));
         fieldValues.put("vulnerabilities", asylumCase.read(VULNERABILITIES_TRIBUNAL_RESPONSE, String.class).orElse(null));
         fieldValues.put("pastExperiences", asylumCase.read(PAST_EXPERIENCES_TRIBUNAL_RESPONSE, String.class).orElse(null));
@@ -58,5 +62,15 @@ public class InternalAdaSuitabilityReviewSuitableLetterTemplate implements Docum
         fieldValues.put("otherHearingRequest", asylumCase.read(ADDITIONAL_TRIBUNAL_RESPONSE, String.class).orElse(null));
 
         return fieldValues;
+    }
+
+    public String formatDateForRendering(
+            String date
+    ) {
+        if (!Strings.isNullOrEmpty(date)) {
+            return LocalDateTime.parse(date).format(DOCUMENT_DATE_FORMAT);
+        }
+
+        return "";
     }
 }
--- src/test/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/utils/DateUtilsTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/utils/DateUtilsTest.java
@@ -13,19 +13,11 @@ import org.mockito.junit.jupiter.MockitoExtension;
 public class DateUtilsTest {
 
     private final LocalDate exampleDateToFormat = LocalDate.parse("2023-03-03");
-    private final String exampleDateTimeToFormat = "2023-06-30T00:00:00.000";
     private final String formattedExampleDate = "3 Mar 2023";
-    private final String formattedExampleDateTime = "30062023";
-
 
     @Test
     void should_format_and_return_date_correctly_from_date() {
         assertEquals(formattedExampleDate, DateUtils.formatDateForNotificationAttachmentDocument(exampleDateToFormat));
-    }
-
-    @Test
-    void should_format_and_return_date_correctly_from_datetime() {
-        assertEquals(formattedExampleDateTime, DateUtils.formatDateTimeForNotificationAttachmentDocument(exampleDateTimeToFormat));
     }
 
 }
