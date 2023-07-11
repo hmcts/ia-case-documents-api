364fd535cde92450c8f6e580770e5d63441de5bb
Neha Aggarwal <neha.aggarwal@version1.com>
2023-07-11 17:56:13 +0100

    RIA-7198: Fix unit tests

--- src/test/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/entities/ccd/EventTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/entities/ccd/EventTest.java
@@ -55,6 +55,6 @@ public class EventTest {
 
     @Test
     public void if_this_test_fails_it_is_because_it_needs_updating_with_your_changes() {
-        assertEquals(45, Event.values().length);
+        assertEquals(44, Event.values().length);
     }
 }
