fedeb3922d68646a2b3f34e0f607b37d126908a2
Ivan Kirsanov <ivan.kirsanov@version1.com>
2023-07-24 14:48:11 +0200

    RIA-7548_internal_appeal_submission_non_ada: styles fix

--- src/main/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/templates/letter/InternalAppealSubmissionNonAdaTemplate.java
+++ src/main/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/templates/letter/InternalAppealSubmissionNonAdaTemplate.java
@@ -17,45 +17,46 @@ import uk.gov.hmcts.reform.iacasedocumentsapi.infrastructure.CustomerServicesPro
 @Component
 public class InternalAppealSubmissionNonAdaTemplate implements DocumentTemplate<AsylumCase> {
 
-        private final String templateName;
-        private final CustomerServicesProvider customerServicesProvider;
-        public InternalAppealSubmissionNonAdaTemplate(
-                @Value("${internalAppealSubmissionNonAdaDocument.templateName}") String templateName,
-                CustomerServicesProvider customerServicesProvider
-        ) {
-                this.templateName = templateName;
-                this.customerServicesProvider = customerServicesProvider;
-        }
+    private final String templateName;
+    private final CustomerServicesProvider customerServicesProvider;
 
-        @Override
-        public String getName() {
-                return templateName;
-        }
+    public InternalAppealSubmissionNonAdaTemplate(
+            @Value("${internalAppealSubmissionNonAdaDocument.templateName}") String templateName,
+            CustomerServicesProvider customerServicesProvider
+    ) {
+        this.templateName = templateName;
+        this.customerServicesProvider = customerServicesProvider;
+    }
 
-        @Override
-        public Map<String, Object> mapFieldValues(CaseDetails<AsylumCase> caseDetails) {
-                final AsylumCase asylumCase = caseDetails.getCaseData();
+    @Override
+    public String getName() {
+        return templateName;
+    }
 
-                final Map<String, Object> fieldValues = new HashMap<>();
+    @Override
+    public Map<String, Object> mapFieldValues(CaseDetails<AsylumCase> caseDetails) {
+        final AsylumCase asylumCase = caseDetails.getCaseData();
 
-                fieldValues.put("hmcts", "[userImage:hmcts.png]");
-                fieldValues.put("customerServicesTelephone", customerServicesProvider.getInternalCustomerServicesTelephone(asylumCase));
-                fieldValues.put("detainedEmail", customerServicesProvider.getInternalCustomerServicesEmail(asylumCase));
-                fieldValues.put("appealReferenceNumber", asylumCase.read(APPEAL_REFERENCE_NUMBER, String.class).orElse(""));
-                fieldValues.put("homeOfficeReferenceNumber", asylumCase.read(HOME_OFFICE_REFERENCE_NUMBER, String.class).orElse(""));
-                fieldValues.put("dateLetterSent", formatDateForNotificationAttachmentDocument(LocalDate.now()));
-                fieldValues.put("appellantGivenNames", asylumCase.read(APPELLANT_GIVEN_NAMES, String.class).orElse(""));
-                fieldValues.put("appellantFamilyName", asylumCase.read(APPELLANT_FAMILY_NAME, String.class).orElse(""));
-                fieldValues.put("dueDate", dueDatePlusFourWeeks(asylumCase));
+        final Map<String, Object> fieldValues = new HashMap<>();
 
-                return fieldValues;
-        }
+        fieldValues.put("hmcts", "[userImage:hmcts.png]");
+        fieldValues.put("customerServicesTelephone", customerServicesProvider.getInternalCustomerServicesTelephone(asylumCase));
+        fieldValues.put("detainedEmail", customerServicesProvider.getInternalCustomerServicesEmail(asylumCase));
+        fieldValues.put("appealReferenceNumber", asylumCase.read(APPEAL_REFERENCE_NUMBER, String.class).orElse(""));
+        fieldValues.put("homeOfficeReferenceNumber", asylumCase.read(HOME_OFFICE_REFERENCE_NUMBER, String.class).orElse(""));
+        fieldValues.put("dateLetterSent", formatDateForNotificationAttachmentDocument(LocalDate.now()));
+        fieldValues.put("appellantGivenNames", asylumCase.read(APPELLANT_GIVEN_NAMES, String.class).orElse(""));
+        fieldValues.put("appellantFamilyName", asylumCase.read(APPELLANT_FAMILY_NAME, String.class).orElse(""));
+        fieldValues.put("dueDate", dueDatePlusFourWeeks(asylumCase));
 
-        private String dueDatePlusFourWeeks(AsylumCase asylumCase) {
-                LocalDate appealSubmissionDate = asylumCase.read(APPEAL_SUBMISSION_DATE, String.class)
-                        .map(LocalDate::parse)
-                        .orElseThrow(() -> new IllegalStateException("appealSubmissionDate is missing"));
+        return fieldValues;
+    }
 
-                return  formatDateForNotificationAttachmentDocument(appealSubmissionDate.plusWeeks(4));
-        }
+    private String dueDatePlusFourWeeks(AsylumCase asylumCase) {
+        LocalDate appealSubmissionDate = asylumCase.read(APPEAL_SUBMISSION_DATE, String.class)
+                .map(LocalDate::parse)
+                .orElseThrow(() -> new IllegalStateException("appealSubmissionDate is missing"));
+
+        return formatDateForNotificationAttachmentDocument(appealSubmissionDate.plusWeeks(4));
+    }
 }
--- src/test/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/handlers/presubmit/AppealSubmissionCreatorTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/handlers/presubmit/AppealSubmissionCreatorTest.java
@@ -8,7 +8,6 @@ import static uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.ccd.field.P
 
 import java.util.Arrays;
 import java.util.Optional;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
