14aa7f958f53ce33a4ec27e92ae67ca28dabe576
Ivan Kirsanov <ivan.kirsanov@version1.com>
2023-08-02 13:21:57 +0200

    RIA-7554_directed_to_upload_bundle: reduce code duplications

--- src/main/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/templates/letter/InternalAppealCanProceedLetterTemplate.java
+++ src/main/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/templates/letter/InternalAppealCanProceedLetterTemplate.java
@@ -19,7 +19,6 @@ public class InternalAppealCanProceedLetterTemplate implements DocumentTemplate<
 
     private final String templateName;
     private final CustomerServicesProvider customerServicesProvider;
-    private int fourWeeksAfterSubmissionDate = 4;
 
     public InternalAppealCanProceedLetterTemplate(
             @Value("${internalAppealCanProceedDocument.templateName}") String templateName,
@@ -45,7 +44,7 @@ public class InternalAppealCanProceedLetterTemplate implements DocumentTemplate<
         fieldValues.put("detainedEmail", customerServicesProvider.getInternalCustomerServicesEmail(asylumCase));
         fieldValues.putAll(getAppellantPersonalisation(asylumCase));
         fieldValues.put("dateLetterSent", formatDateForNotificationAttachmentDocument(LocalDate.now()));
-        fieldValues.put("dueDate", dueDatePlusNumberOfWeeks(asylumCase, fourWeeksAfterSubmissionDate));
+        fieldValues.put("dueDate", dueDatePlusNumberOfWeeks(asylumCase, 4));
 
         return fieldValues;
     }
