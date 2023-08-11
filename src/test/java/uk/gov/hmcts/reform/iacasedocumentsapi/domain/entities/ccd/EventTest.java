220d54dc0ad6305681f3c9a05aace58d7bc6328c
Ivan Kirsanov <ivan.kirsanov@version1.com>
2023-08-11 16:37:18 +0200

    RIA-7556_internal_det_ho_review_maintaned: Fix on functional test

--- src/test/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/entities/ccd/EventTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/entities/ccd/EventTest.java
@@ -58,6 +58,6 @@ public class EventTest {
 
     @Test
     public void if_this_test_fails_it_is_because_it_needs_updating_with_your_changes() {
-        assertEquals(48, Event.values().length);
+        assertEquals(49, Event.values().length);
     }
 }
