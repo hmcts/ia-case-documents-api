55435294e4e833baa8cad04b76679f4333df3f50
rajeshthuraiyur <rajesh.thuraiyur@gmail.com>
2023-09-11 13:16:58 +0100

    Updates to fix FT failures

--- src/functionalTest/java/uk/gov/hmcts/reform/iacasedocumentsapi/CcdScenarioRunnerTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/iacasedocumentsapi/CcdScenarioRunnerTest.java
@@ -1,6 +1,8 @@
 package uk.gov.hmcts.reform.iacasedocumentsapi;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -11,14 +13,17 @@ import java.time.LocalDateTime;
 import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.StreamSupport;
+import lombok.SneakyThrows;
 import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
 import net.serenitybdd.rest.SerenityRest;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.core.env.AbstractEnvironment;
 import org.springframework.core.env.EnumerablePropertySource;
 import org.springframework.core.env.Environment;
@@ -28,6 +33,7 @@ import org.springframework.test.context.ActiveProfiles;
 import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.AsylumCase;
 import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.ccd.callback.PreSubmitCallbackResponse;
 import uk.gov.hmcts.reform.iacasedocumentsapi.fixtures.Fixture;
+import uk.gov.hmcts.reform.iacasedocumentsapi.infrastructure.security.RequestUserAccessTokenProvider;
 import uk.gov.hmcts.reform.iacasedocumentsapi.util.*;
 import uk.gov.hmcts.reform.iacasedocumentsapi.verifiers.Verifier;
 
@@ -45,11 +51,24 @@ public class CcdScenarioRunnerTest {
     @Autowired private List<Fixture> fixtures;
     @Autowired private List<Verifier> verifiers;
 
+    @MockBean
+    static RequestUserAccessTokenProvider requestUserAccessTokenProvider;
+
+    private static String accessToken = null;
+
+    @BeforeAll
+    @SneakyThrows
+    public static void authenticateMe(@Autowired AuthorizationHeadersProvider authorizationHeadersProvider) {
+        accessToken = authorizationHeadersProvider.getCaseOfficerAuthorization().getValue("Authorization");
+        Thread.sleep(1000);
+        assertNotNull(accessToken);
+    }
     @BeforeEach
     public void setup() {
         MapSerializer.setObjectMapper(objectMapper);
         RestAssured.baseURI = targetInstance;
         RestAssured.useRelaxedHTTPSValidation();
+        when(requestUserAccessTokenProvider.getAccessToken()).thenReturn(accessToken);
     }
 
     @Test
@@ -311,6 +330,23 @@ public class CcdScenarioRunnerTest {
 
             return authorizationHeadersProvider
                 .getJudgeAuthorization();
+        }
+
+        if ("System".equalsIgnoreCase(credentials)) {
+            return authorizationHeadersProvider
+                .getSystemAuthorization();
+        }
+
+        if ("HomeOfficeLart".equalsIgnoreCase(credentials)) {
+
+            return authorizationHeadersProvider
+                .getHomeOfficeLartAuthorization();
+        }
+
+        if ("HomeOfficePOU".equalsIgnoreCase(credentials)) {
+
+            return authorizationHeadersProvider
+                .getHomeOfficePouAuthorization();
         }
 
         return new Headers();
