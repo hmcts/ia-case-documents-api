56f012a43e635525e6d2fe5042c3bcd1d22a4c01
rajeshthuraiyur <rajesh.thuraiyur@gmail.com>
2023-09-12 09:45:40 +0100

    update to fix sonar bugs

--- src/functionalTest/java/uk/gov/hmcts/reform/iacasedocumentsapi/util/AuthorizationHeadersProvider.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/iacasedocumentsapi/util/AuthorizationHeadersProvider.java
@@ -257,7 +257,7 @@ public class AuthorizationHeadersProvider {
             new Header("Authorization", accessToken)
         );
     }
-    
+
     public Headers getSystemAuthorization() {
 
         MultiValueMap<String, String> tokenRequestForm = new LinkedMultiValueMap<>();
