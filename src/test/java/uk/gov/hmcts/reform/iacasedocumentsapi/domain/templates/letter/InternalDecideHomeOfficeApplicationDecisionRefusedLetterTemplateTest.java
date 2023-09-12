7ee94d446798a48298989bab46e0f061f14cc058
hussain-faruk <118450580+hussain-faruk@users.noreply.github.com>
2023-09-12 10:13:11 +0100

    RIA-7368 (#690)
    * Addedn functionality to generate letter when HO application decided
    * Refactored RIA-7365
    * Added/updated unit/FTs

--- src/test/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/templates/letter/InternalDecideHomeOfficeApplicationDecisionRefusedLetterTemplateTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/templates/letter/InternalDecideHomeOfficeApplicationDecisionRefusedLetterTemplateTest.java
@@ -4,25 +4,24 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.AsylumCaseDefinition.*;
+import static uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.AsylumCaseDefinition.DECIDE_AN_APPLICATION_ID;
 import static uk.gov.hmcts.reform.iacasedocumentsapi.domain.utils.DateUtils.formatDateForNotificationAttachmentDocument;
 
 import java.time.LocalDate;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EnumSource;
-import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
 import uk.gov.hmcts.reform.iacasedocumentsapi.domain.DateProvider;
-import uk.gov.hmcts.reform.iacasedocumentsapi.domain.UserDetailsProvider;
 import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.AsylumCase;
 import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.MakeAnApplication;
-import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.UserDetails;
 import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.ccd.CaseDetails;
 import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.ccd.State;
 import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.ccd.field.IdValue;
@@ -30,10 +29,11 @@ import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.ccd.field.YesOrNo;
 import uk.gov.hmcts.reform.iacasedocumentsapi.infrastructure.CustomerServicesProvider;
 import uk.gov.hmcts.reform.iacasedocumentsapi.infrastructure.MakeAnApplicationService;
 
+
 @ExtendWith(MockitoExtension.class)
 @SuppressWarnings("unchecked")
 @MockitoSettings(strictness = Strictness.LENIENT)
-public class InternalDecideAnApplicationDecisionRefusedLetterTemplateTest {
+public class InternalDecideHomeOfficeApplicationDecisionRefusedLetterTemplateTest {
 
     @Mock
     private CaseDetails<AsylumCase> caseDetails;
@@ -45,14 +45,6 @@ public class InternalDecideAnApplicationDecisionRefusedLetterTemplateTest {
     private CustomerServicesProvider customerServicesProvider;
     @Mock
     private MakeAnApplicationService makeAnApplicationService;
-    @Mock
-    UserDetailsProvider userDetailsProvider;
-    @Mock
-    UserDetails userDetails;
-    private final String judgeRole = "caseworker-ia-iacjudge";
-    private final String legalOfficerRole = "caseworker-ia-caseofficer";
-    private final String applicationRefusedAdaFormName = "IAFT-ADA4: Make an application – Accelerated detained appeal (ADA)";
-    private final String applicationRefusedDetainedNonAdaFormName = "IAFT-DE4: Make an application – Detained appeal";
     private final String templateName = "TB-IAC-DEC-ENG-00016.docx";
     private final String internalAdaCustomerServicesTelephoneNumber = "0300 123 1711";
     private final String internalAdaCustomerServicesEmailAddress = "IAC-ADA-HW@justice.gov.uk";
@@ -70,17 +62,16 @@ public class InternalDecideAnApplicationDecisionRefusedLetterTemplateTest {
             "Refused",
             State.APPEAL_SUBMITTED.toString(),
             "caseworker-ia-admofficer");
-    private InternalDecideAnApplicationDecisionRefusedLetterTemplate internalDecideAnApplicationDecisionRefusedLetterTemplate;
+    private InternalDecideHomeOfficeApplicationDecisionRefusedLetterTemplate internalDecideHomeOfficeApplicationDecisionRefusedLetterTemplate;
 
     @BeforeEach
     void setUp() {
-        internalDecideAnApplicationDecisionRefusedLetterTemplate =
-                new InternalDecideAnApplicationDecisionRefusedLetterTemplate(
+        internalDecideHomeOfficeApplicationDecisionRefusedLetterTemplate =
+                new InternalDecideHomeOfficeApplicationDecisionRefusedLetterTemplate(
                         templateName,
                         dateProvider,
                         customerServicesProvider,
-                        makeAnApplicationService,
-                        userDetailsProvider
+                        makeAnApplicationService
                 );
     }
 
@@ -99,9 +90,6 @@ public class InternalDecideAnApplicationDecisionRefusedLetterTemplateTest {
 
         when(dateProvider.now()).thenReturn(LocalDate.now());
 
-        when(userDetailsProvider.getUserDetails()).thenReturn(userDetails);
-        when(userDetails.getRoles()).thenReturn(List.of(judgeRole));
-
         makeAnApplications.add(new IdValue<>("1", application));
 
         when(asylumCase.read(MAKE_AN_APPLICATIONS)).thenReturn(Optional.of(makeAnApplications));
@@ -112,19 +100,16 @@ public class InternalDecideAnApplicationDecisionRefusedLetterTemplateTest {
 
     @Test
     void should_return_template_name() {
-        assertEquals(templateName, internalDecideAnApplicationDecisionRefusedLetterTemplate.getName());
+        assertEquals(templateName, internalDecideHomeOfficeApplicationDecisionRefusedLetterTemplate.getName());
     }
 
-    @ParameterizedTest
-    @ValueSource(strings = {judgeRole, legalOfficerRole})
-    void should_map_case_data_to_template_field_values_and_map_decision_maker_field_correctly(String role) {
+    @Test
+    void should_map_case_data_to_template_field_values() {
         dataSetup();
 
-        when(userDetailsProvider.getUserDetails().getRoles()).thenReturn(List.of(role));
+        Map<String, Object> templateFieldValues = internalDecideHomeOfficeApplicationDecisionRefusedLetterTemplate.mapFieldValues(caseDetails);
 
-        Map<String, Object> templateFieldValues = internalDecideAnApplicationDecisionRefusedLetterTemplate.mapFieldValues(caseDetails);
-
-        assertEquals(12, templateFieldValues.size());
+        assertEquals(10, templateFieldValues.size());
         assertEquals("[userImage:hmcts.png]", templateFieldValues.get("hmcts"));
         assertEquals(appealReferenceNumber, templateFieldValues.get("appealReferenceNumber"));
         assertEquals(homeOfficeReferenceNumber, templateFieldValues.get("homeOfficeReferenceNumber"));
@@ -135,41 +120,6 @@ public class InternalDecideAnApplicationDecisionRefusedLetterTemplateTest {
         assertEquals(formatDateForNotificationAttachmentDocument(dateProvider.now()), templateFieldValues.get("dateLetterSent"));
         assertEquals(application.getType(), templateFieldValues.get("applicationType"));
         assertEquals(application.getDecisionReason(), templateFieldValues.get("applicationReason"));
-        assertEquals(applicationRefusedDetainedNonAdaFormName, templateFieldValues.get("formName"));
-
-        if (role.equals(judgeRole)) {
-            assertEquals("Judge", templateFieldValues.get("decisionMaker"));
-        } else if (role.equals(legalOfficerRole)) {
-            assertEquals("Legal Officer", templateFieldValues.get("decisionMaker"));
-        }
-    }
-
-    @ParameterizedTest
-    @EnumSource(value = YesOrNo.class)
-    void should_map_case_data_to_template_field_values_and_map_decision_maker_field_correctly(YesOrNo yesOrNo) {
-        dataSetup();
-        when(asylumCase.read(IS_ACCELERATED_DETAINED_APPEAL, YesOrNo.class)).thenReturn(Optional.of(yesOrNo));
-        Map<String, Object> templateFieldValues = internalDecideAnApplicationDecisionRefusedLetterTemplate.mapFieldValues(caseDetails);
-
-        assertEquals(12, templateFieldValues.size());
-        assertEquals("[userImage:hmcts.png]", templateFieldValues.get("hmcts"));
-        assertEquals(appealReferenceNumber, templateFieldValues.get("appealReferenceNumber"));
-        assertEquals(homeOfficeReferenceNumber, templateFieldValues.get("homeOfficeReferenceNumber"));
-        assertEquals(appellantGivenNames, templateFieldValues.get("appellantGivenNames"));
-        assertEquals(appellantFamilyName, templateFieldValues.get("appellantFamilyName"));
-        assertEquals(internalAdaCustomerServicesTelephoneNumber, templateFieldValues.get("customerServicesTelephone"));
-        assertEquals(internalAdaCustomerServicesEmailAddress, templateFieldValues.get("customerServicesEmail"));
-        assertEquals(formatDateForNotificationAttachmentDocument(dateProvider.now()), templateFieldValues.get("dateLetterSent"));
-        assertEquals(application.getType(), templateFieldValues.get("applicationType"));
-        assertEquals(application.getDecisionReason(), templateFieldValues.get("applicationReason"));
-        assertEquals("Judge", templateFieldValues.get("decisionMaker"));
-
-
-        if (yesOrNo.equals(YesOrNo.YES)) {
-            assertEquals(applicationRefusedAdaFormName, templateFieldValues.get("formName"));
-        } else {
-            assertEquals(applicationRefusedDetainedNonAdaFormName, templateFieldValues.get("formName"));
-        }
     }
 
     @Test
@@ -192,7 +142,7 @@ public class InternalDecideAnApplicationDecisionRefusedLetterTemplateTest {
 
         when(makeAnApplicationService.getMakeAnApplication(asylumCase, true)).thenReturn(Optional.of(testApplication));
 
-        assertThatThrownBy(() -> internalDecideAnApplicationDecisionRefusedLetterTemplate.mapFieldValues(caseDetails))
+        assertThatThrownBy(() -> internalDecideHomeOfficeApplicationDecisionRefusedLetterTemplate.mapFieldValues(caseDetails))
                 .hasMessage("Application type could not be parsed")
                 .isExactlyInstanceOf(IllegalStateException.class);
     }
