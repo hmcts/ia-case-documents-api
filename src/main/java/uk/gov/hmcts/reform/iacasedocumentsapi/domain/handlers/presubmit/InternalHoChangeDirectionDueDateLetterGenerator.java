5021e5dcd1591e4931d00d32d412c57d076f5a26
Ivan Kirsanov <ivan.kirsanov@version1.com>
2023-10-02 12:15:16 +0200

    RIA-7439_ho_direction_due_date: Move method isPartyIsRespondent ot Utils

--- src/main/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/handlers/presubmit/InternalHoChangeDirectionDueDateLetterGenerator.java
+++ src/main/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/handlers/presubmit/InternalHoChangeDirectionDueDateLetterGenerator.java
@@ -1,15 +1,13 @@
 package uk.gov.hmcts.reform.iacasedocumentsapi.domain.handlers.presubmit;
 
-import static uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.AsylumCaseDefinition.*;
-import static uk.gov.hmcts.reform.iacasedocumentsapi.domain.utils.AsylumCaseUtils.isAppellantInDetention;
-import static uk.gov.hmcts.reform.iacasedocumentsapi.domain.utils.AsylumCaseUtils.isInternalCase;
+import static uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.AsylumCaseDefinition.NOTIFICATION_ATTACHMENT_DOCUMENTS;
+import static uk.gov.hmcts.reform.iacasedocumentsapi.domain.utils.AsylumCaseUtils.*;
 
 import java.util.Objects;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.AsylumCase;
 import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.DocumentTag;
-import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.Parties;
 import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.ccd.CaseDetails;
 import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.ccd.Event;
 import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.ccd.callback.Callback;
@@ -74,9 +72,4 @@ public class InternalHoChangeDirectionDueDateLetterGenerator implements PreSubmi
         return new PreSubmitCallbackResponse<>(asylumCase);
     }
 
-    private boolean isDirectionPartyRespondent(AsylumCase asylumCase) {
-        return asylumCase.read(DIRECTION_EDIT_PARTIES, Parties.class)
-                .map(parties -> parties.equals(Parties.RESPONDENT))
-                .orElse(false);
-    }
 }
