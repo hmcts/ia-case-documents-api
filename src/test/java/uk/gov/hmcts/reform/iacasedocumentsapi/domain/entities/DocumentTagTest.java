c69dfaab2fa2b6772419e8192e2cc24c1400b806
Ivan Kirsanov <ivan.kirsanov@version1.com>
2023-10-03 09:58:30 +0200

    RIA-7439_ho_direction_due_date: merge conflicts resolve

--- src/test/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/entities/DocumentTagTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/entities/DocumentTagTest.java
@@ -80,6 +80,6 @@ class DocumentTagTest {
 
     @Test
     void if_this_test_fails_it_is_because_it_needs_updating_with_your_changes() {
-        assertEquals(75, DocumentTag.values().length);
+        assertEquals(76, DocumentTag.values().length);
     }
 }
