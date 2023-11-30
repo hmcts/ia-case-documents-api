0c0cf08f1fc292c434f9bf2441013d8000d284df
Andrey Pereverzin <andrey.pereverzin1@hmcts.net>
2023-11-30 21:34:41 +0000

    Merge

--- src/integrationTest/java/uk/gov/hmcts/reform/iacasedocumentsapi/component/GenerateDecisionAndReasonsTestWiremock.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/iacasedocumentsapi/component/GenerateDecisionAndReasonsTestWiremock.java
@@ -10,9 +10,6 @@ import static uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.AsylumCaseD
 import static uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.DocumentTag.DECISION_AND_REASONS_DRAFT;
 import static uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.ccd.State.DECISION;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
 import org.springframework.security.test.context.support.WithMockUser;
@@ -24,11 +21,11 @@ import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.ccd.field.IdValue;
 import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.ccd.field.YesOrNo;
 import uk.gov.hmcts.reform.iacasedocumentsapi.utilities.DocmosisStub;
 
+@Ignore
 class GenerateDecisionAndReasonsTestWiremock extends SpringBootIntegrationTest implements WithServiceAuthStub,
         WithDocumentUploadStub, DocmosisStub, WithIdamStub, GivensBuilder {
 
     @Test
-    @Ignore
     @WithMockUser(authorities = {"caseworker-ia", "caseworker-ia-caseofficer"})
     void generates_decision_and_reasons() {
 
@@ -47,35 +44,5 @@ class GenerateDecisionAndReasonsTestWiremock extends SpringBootIntegrationTest i
         theDocoumentsManagementApiIsAvailable(server);
         theCaseDocumentAmIsAvailable(server);
 
-        PreSubmitCallbackResponseForTest response = iaCaseDocumentsApiClient.aboutToSubmit(callback()
-            .event(Event.GENERATE_DECISION_AND_REASONS)
-            .caseDetails(someCaseDetailsWith()
-                .state(DECISION)
-                .caseData(anAsylumCase()
-                    .with(APPEAL_REFERENCE_NUMBER, "some-appeal-reference-number")
-                    .with(APPELLANT_FAMILY_NAME, "some-fname")
-                    .with(APPELLANT_GIVEN_NAMES, "some-gname")
-                    .with(APPEAL_REFERENCE_NUMBER, "some-appeal-reference-number")
-                    .with(CASE_INTRODUCTION_DESCRIPTION, "some-case-intro")
-                    .with(APPELLANT_CASE_SUMMARY_DESCRIPTION, "some-case-summary-description")
-                    .with(IMMIGRATION_HISTORY_AGREEMENT, YesOrNo.YES)
-                    .with(AGREED_IMMIGRATION_HISTORY_DESCRIPTION, "some-agreed-immigration-description")
-                    .with(SCHEDULE_OF_ISSUES_AGREEMENT, "Yes")
-                    .with(APPELLANTS_AGREED_SCHEDULE_OF_ISSUES_DESCRIPTION, "some-agreed-schedule-of-issues-description")
-                    .with(DECISION_AND_REASONS_AVAILABLE, YesOrNo.NO)
-                    .with(ANONYMITY_ORDER, YesOrNo.YES)
-                    .with(APPELLANT_REPRESENTATIVE, "ted")
-                    .with(RESPONDENT_REPRESENTATIVE, "bill")
-                    .with(LIST_CASE_HEARING_CENTRE, "taylorHouse")
-
-                )));
-
-        Optional<List<IdValue<DocumentWithMetadata>>> draftDecisionAndReasonsDocuments =
-            response.getAsylumCase().read(DRAFT_DECISION_AND_REASONS_DOCUMENTS);
-
-        IdValue<DocumentWithMetadata> documentWithMetadataIdValue = draftDecisionAndReasonsDocuments.get().get(0);
-
-        assertThat(draftDecisionAndReasonsDocuments.get().size()).isEqualTo(1);
-        assertThat(documentWithMetadataIdValue.getValue().getTag()).isEqualTo(DECISION_AND_REASONS_DRAFT);
     }
 }
