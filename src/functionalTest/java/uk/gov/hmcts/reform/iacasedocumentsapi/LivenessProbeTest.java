372217458d89ebeef14a4e6ed424a60e549e756e
Andrey Pereverzin <andrey.pereverzin1@hmcts.net>
2023-12-22 17:18:12 +0000

    Fix tests

--- src/functionalTest/java/uk/gov/hmcts/reform/iacasedocumentsapi/LivenessProbeTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/iacasedocumentsapi/LivenessProbeTest.java
@@ -5,39 +5,43 @@ import static org.assertj.core.api.Assertions.assertThat;
 import io.restassured.RestAssured;
 import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
 import net.serenitybdd.rest.SerenityRest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 
 @RunWith(SpringIntegrationSerenityRunner.class)
 @SpringBootTest
 @ActiveProfiles("functional")
- class WelcomeTest {
+class LivenessProbeTest {
 
     @Value("${targetInstance}") private String targetInstance;
 
-    @Test
-     void should_welcome_with_200_response_code() {
-
+    @BeforeEach
+    void setUp() {
         RestAssured.baseURI = targetInstance;
         RestAssured.useRelaxedHTTPSValidation();
-
-        String response =
-            SerenityRest
-                .given()
-                .when()
-                .get("/")
-                .then()
-                .statusCode(HttpStatus.OK.value())
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .and()
-                .extract().body().asString();
-
-        assertThat(response)
-            .contains("Welcome to Immigration & Asylum case documents API");
     }
+
+    @Test
+    void should_ping_liveness_endpoint_and_get_ok() {
+
+        String response = SerenityRest
+            .given()
+            .when()
+            .get("/health/liveness")
+            .then()
+            .statusCode(HttpStatus.OK.value())
+            .log().all()
+            .and()
+            .extract().body()
+            .jsonPath().get("status");
+
+        assertThat(response).isEqualTo("UP");
+
+    }
+
 }
