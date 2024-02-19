3d6bb32c9888ca48620b5bf1f5602bcda92379fe
NehaAggarwal <neha.aggarwal@version1.com>
2024-02-19 16:39:23 +0000

    RIA-8232 Added new field RECORD_THE_DECISION_LIST_IMA to hold the decision once the IMA feature is turned on (#832)

--- src/test/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/entities/BailCaseFieldDefinitionTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/entities/BailCaseFieldDefinitionTest.java
@@ -32,6 +32,6 @@ public class BailCaseFieldDefinitionTest {
 
     @Test
     public void should_fail_if_new_fields_added_in_class() {
-        assertEquals(148, values().length);
+        assertEquals(149, values().length);
     }
 }
