9682a5b069b2b616d2185944840ed24cbff74c39
ThomasKKC <99262639+ThomasKKC@users.noreply.github.com>
2024-02-29 16:47:01 +0000

    RIA-8721 Added AMENDED cover letter for updateTribunalDecision event (#837)
    * RIA-8721 Added AMENDED cover letter for updateTribunalDecision event
    
    * RIA-8721 Added unit test
    
    * RIA-8721 Fix checkstyle

---
+++ src/main/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/handlers/presubmit/UpdateTribunalDecisionCreator.java
@@ -0,0 +1,87 @@
+package uk.gov.hmcts.reform.iacasedocumentsapi.domain.handlers.presubmit;
+
+import static java.util.Objects.requireNonNull;
+import static uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.AsylumCaseDefinition.*;
+import static uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.UpdateTribunalRules.UNDER_RULE_31;
+import static uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.ccd.field.JourneyType.AIP;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.AsylumCase;
+import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.DocumentTag;
+import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.UpdateTribunalRules;
+import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.ccd.CaseDetails;
+import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.ccd.Event;
+import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.ccd.callback.Callback;
+import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.ccd.callback.PreSubmitCallbackResponse;
+import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.ccd.callback.PreSubmitCallbackStage;
+import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.ccd.field.Document;
+import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.ccd.field.JourneyType;
+import uk.gov.hmcts.reform.iacasedocumentsapi.domain.handlers.PreSubmitCallbackHandler;
+import uk.gov.hmcts.reform.iacasedocumentsapi.domain.service.DocumentCreator;
+import uk.gov.hmcts.reform.iacasedocumentsapi.domain.service.DocumentHandler;
+
+@Component
+public class UpdateTribunalDecisionCreator implements PreSubmitCallbackHandler<AsylumCase> {
+    private final DocumentCreator<AsylumCase> updatedDecisionAndReasonsCoverLetterDocumentCreator;
+    private final DocumentCreator<AsylumCase> aipUpdatedDecisionAndReasonsCoverLetterDocumentCreator;
+    private final DocumentHandler documentHandler;
+
+
+    public UpdateTribunalDecisionCreator(
+            @Qualifier("updatedDecisionAndReasonsCoverLetter") DocumentCreator<AsylumCase> updatedDecisionAndReasonsCoverLetterDocumentCreator,
+            @Qualifier("aipUpdatedDecisionAndReasonsCoverLetter") DocumentCreator<AsylumCase> aipUpdatedDecisionAndReasonsCoverLetterDocumentCreator,
+            DocumentHandler documentHandler
+    ) {
+        this.updatedDecisionAndReasonsCoverLetterDocumentCreator = updatedDecisionAndReasonsCoverLetterDocumentCreator;
+        this.aipUpdatedDecisionAndReasonsCoverLetterDocumentCreator = aipUpdatedDecisionAndReasonsCoverLetterDocumentCreator;
+        this.documentHandler = documentHandler;
+    }
+
+    public boolean canHandle(
+        PreSubmitCallbackStage callbackStage,
+        Callback<AsylumCase> callback
+    ) {
+        requireNonNull(callbackStage, "callbackStage must not be null");
+        requireNonNull(callback, "callback must not be null");
+
+        return callbackStage == PreSubmitCallbackStage.ABOUT_TO_SUBMIT
+                && isDecisionRule31(callback.getCaseDetails().getCaseData())
+                && callback.getEvent() == Event.UPDATE_TRIBUNAL_DECISION;
+    }
+
+    public PreSubmitCallbackResponse<AsylumCase> handle(
+        PreSubmitCallbackStage callbackStage,
+        Callback<AsylumCase> callback
+    ) {
+        if (!canHandle(callbackStage, callback)) {
+            throw new IllegalStateException("Cannot handle callback");
+        }
+
+        final CaseDetails<AsylumCase> caseDetails = callback.getCaseDetails();
+        final AsylumCase asylumCase = caseDetails.getCaseData();
+        Document coverLetter = getCoverLetterCreatorInstance(asylumCase).create(caseDetails);
+
+        documentHandler.addWithMetadataWithoutReplacingExistingDocuments(
+                asylumCase,
+                coverLetter,
+                FINAL_DECISION_AND_REASONS_DOCUMENTS,
+                DocumentTag.DECISION_AND_REASONS_COVER_LETTER
+        );
+
+        return new PreSubmitCallbackResponse<>(asylumCase);
+    }
+
+    private boolean isDecisionRule31(AsylumCase asylumCase) {
+        return asylumCase.read(UPDATE_TRIBUNAL_DECISION_LIST, UpdateTribunalRules.class)
+                .map(type -> type.equals(UNDER_RULE_31)).orElse(false);
+    }
+
+    private DocumentCreator<AsylumCase> getCoverLetterCreatorInstance(AsylumCase asylumCase) {
+        boolean isAipJourney = asylumCase.read(JOURNEY_TYPE, JourneyType.class)
+                .map(type -> type == AIP).orElse(false);
+
+        return isAipJourney ? aipUpdatedDecisionAndReasonsCoverLetterDocumentCreator :
+                updatedDecisionAndReasonsCoverLetterDocumentCreator;
+    }
+}
---
+++ src/main/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/templates/UpdatedTribunalDecisionAndReasonsCoverLetterTemplate.java
@@ -0,0 +1,57 @@
+package uk.gov.hmcts.reform.iacasedocumentsapi.domain.templates;
+
+import static uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.AsylumCaseDefinition.APPEAL_REFERENCE_NUMBER;
+import static uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.AsylumCaseDefinition.APPELLANT_FAMILY_NAME;
+import static uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.AsylumCaseDefinition.APPELLANT_GIVEN_NAMES;
+import static uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.AsylumCaseDefinition.HOME_OFFICE_REFERENCE_NUMBER;
+import static uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.AsylumCaseDefinition.LEGAL_REP_REFERENCE_NUMBER;
+import static uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.AsylumCaseDefinition.UPDATED_APPEAL_DECISION;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.AsylumCase;
+import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.ccd.CaseDetails;
+import uk.gov.hmcts.reform.iacasedocumentsapi.infrastructure.CustomerServicesProvider;
+
+@Component
+public class UpdatedTribunalDecisionAndReasonsCoverLetterTemplate implements DocumentTemplate<AsylumCase> {
+
+    private final String templateName;
+    private final CustomerServicesProvider customerServicesProvider;
+
+    public UpdatedTribunalDecisionAndReasonsCoverLetterTemplate(
+        @Value("${decisionAndReasonsCoverLetter.templateName}") String templateName,
+        CustomerServicesProvider customerServicesProvider
+    ) {
+        this.templateName = templateName;
+        this.customerServicesProvider = customerServicesProvider;
+    }
+
+    public String getName() {
+        return templateName;
+    }
+
+    public Map<String, Object> mapFieldValues(
+        CaseDetails<AsylumCase> caseDetails
+    ) {
+        final AsylumCase asylumCase = caseDetails.getCaseData();
+
+        final Map<String, Object> fieldValues = new HashMap<>();
+
+        fieldValues.put("hmcts", "[userImage:hmcts.png]");
+        fieldValues.put("appealReferenceNumber", asylumCase.read(APPEAL_REFERENCE_NUMBER, String.class).orElse(""));
+        fieldValues.put("homeOfficeReferenceNumber", asylumCase.read(HOME_OFFICE_REFERENCE_NUMBER, String.class).orElse(""));
+        fieldValues.put("legalRepReferenceNumber", asylumCase.read(LEGAL_REP_REFERENCE_NUMBER, String.class).orElse(""));
+        fieldValues.put("appellantGivenNames", asylumCase.read(APPELLANT_GIVEN_NAMES, String.class).orElse(""));
+        fieldValues.put("appellantFamilyName", asylumCase.read(APPELLANT_FAMILY_NAME, String.class).orElse(""));
+        fieldValues.put("allowed", asylumCase.read(UPDATED_APPEAL_DECISION, String.class)
+                .orElseThrow(() -> new IllegalStateException("update appeal decision must be present"))
+                .equals("Allowed") ? "Yes" : "No");
+        fieldValues.put("customerServicesTelephone", customerServicesProvider.getCustomerServicesTelephone());
+        fieldValues.put("customerServicesEmail", customerServicesProvider.getCustomerServicesEmail());
+
+        return fieldValues;
+    }
+}
