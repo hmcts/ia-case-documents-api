500e562cb6e3390023e7565c357150b211dabf69
DennisHoV1 <dennis.ho@hmcts.net>
2024-03-06 10:26:21 +0000

    update the logic for code review feedback

--- src/test/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/templates/UpdatedTribunalAipDecisionAndReasonsCoverLetterTemplateTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/templates/UpdatedTribunalAipDecisionAndReasonsCoverLetterTemplateTest.java
@@ -1,15 +1,11 @@
 package uk.gov.hmcts.reform.iacasedocumentsapi.domain.templates;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
-import java.util.HashMap;
-import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.AsylumCase;
@@ -20,12 +16,6 @@ import uk.gov.hmcts.reform.iacasedocumentsapi.domain.templates.helper.UpdatedTri
 class UpdatedTribunalAipDecisionAndReasonsCoverLetterTemplateTest {
 
     private final String templateName = "";
-    private final String someAppealReferenceNumber = "appealReferenceNumber";
-    private final String someHomeOfficeReferenceNumber = "homeOfficeRef";
-    private final String someGivenNames = "some-given-name";
-    private final String someFamilyName = "some-family-name";
-    private String customerServicesTelephone = "555 555 555";
-    private String customerServicesEmail = "customer.services@example.com";
     @Mock
     private CaseDetails<AsylumCase> caseDetails;
     @Mock
@@ -41,35 +31,11 @@ class UpdatedTribunalAipDecisionAndReasonsCoverLetterTemplateTest {
         );
     }
 
-    @ParameterizedTest
-    @ValueSource(strings = {"Allowed", "Dismissed"})
-    void returns_correctly_mapped_template_values(String updatedAppealDecision) {
+    @Test
+    void returns_correctly_mapped_template_values() {
 
-        Map<String, Object> fieldValues = new HashMap<>();
-        fieldValues.put("appealReferenceNumber", someAppealReferenceNumber);
-        fieldValues.put("homeOfficeReferenceNumber", someHomeOfficeReferenceNumber);
-        fieldValues.put("appellantGivenNames", someGivenNames);
-        fieldValues.put("appellantFamilyName", someFamilyName);
-        fieldValues.put("allowed", updatedAppealDecision.equals("Allowed") ? "Yes" : "No");
-        fieldValues.put("customerServicesTelephone", customerServicesTelephone);
-        fieldValues.put("customerServicesEmail", customerServicesEmail);
-
-        when(templateHelper.getCommonMapFieldValues(caseDetails)).thenReturn(fieldValues);
-
-        Map<String, Object> templateValues = updatedTribunalAipDecisionAndReasonsCoverLetterTemplate.mapFieldValues(caseDetails);
-
-        assertEquals(7, templateValues.size());
-        assertEquals(templateValues.get("appealReferenceNumber"), someAppealReferenceNumber);
-        assertEquals(templateValues.get("homeOfficeReferenceNumber"), someHomeOfficeReferenceNumber);
-        assertEquals(templateValues.get("appellantGivenNames"), someGivenNames);
-        assertEquals(templateValues.get("appellantFamilyName"), someFamilyName);
-        if (updatedAppealDecision.equals("Allowed")) {
-            assertEquals(templateValues.get("allowed"), "Yes");
-        } else {
-            assertEquals(templateValues.get("allowed"), "No");
-        }
-        assertEquals(customerServicesTelephone, fieldValues.get("customerServicesTelephone"));
-        assertEquals(customerServicesEmail, fieldValues.get("customerServicesEmail"));
+        updatedTribunalAipDecisionAndReasonsCoverLetterTemplate.mapFieldValues(caseDetails);
+        verify(templateHelper, times(1)).getCommonMapFieldValues(caseDetails);
     }
 
     @Test
--- src/test/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/templates/UpdatedTribunalDecisionAndReasonsCoverLetterTemplateTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/templates/UpdatedTribunalDecisionAndReasonsCoverLetterTemplateTest.java
@@ -1,17 +1,14 @@
 package uk.gov.hmcts.reform.iacasedocumentsapi.domain.templates;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.AsylumCaseDefinition.LEGAL_REP_REFERENCE_NUMBER;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.AsylumCase;
@@ -26,14 +23,7 @@ public class UpdatedTribunalDecisionAndReasonsCoverLetterTemplateTest {
     @Mock private UpdatedTribunalDecisionAndReasonsCoverLetterTemplateHelper templateHelper;
 
     private String templateName = "some-template-name.docx";
-    private String someAppealReferenceNumber = "some-appeal-ref";
-    private String someHomeOfficeReferenceNumber = "some-home-office-ref";
     private String someLegalRepReferenceNumber = "some-legal-rep-ref";
-    private String someGivenNames = "some-given-name";
-    private String someFamilyName = "some-family-name";
-
-    private String customerServicesTelephone = "555 555 555";
-    private String customerServicesEmail = "customer.services@example.com";
 
     private UpdatedTribunalDecisionAndReasonsCoverLetterTemplate updatedTribunalDecisionAndReasonsCoverLetterTemplate;
 
@@ -46,39 +36,16 @@ public class UpdatedTribunalDecisionAndReasonsCoverLetterTemplateTest {
         );
     }
 
-    @ParameterizedTest
-    @ValueSource(strings = {"Allowed", "Dismissed"})
-    public void returns_correctly_mapped_template_values(String updatedAppealDecisionn) {
+    @Test
+    public void returns_correctly_mapped_template_values() {
 
         when(asylumCase.read(LEGAL_REP_REFERENCE_NUMBER, String.class)).thenReturn(Optional.of(someLegalRepReferenceNumber));
-        Map<String, Object> fieldValues = new HashMap<>();
-        fieldValues.put("appealReferenceNumber", someAppealReferenceNumber);
-        fieldValues.put("homeOfficeReferenceNumber", someHomeOfficeReferenceNumber);
-        fieldValues.put("appellantGivenNames", someGivenNames);
-        fieldValues.put("appellantFamilyName", someFamilyName);
-        fieldValues.put("allowed", updatedAppealDecisionn.equals("Allowed") ? "Yes" : "No");
-        fieldValues.put("customerServicesTelephone", customerServicesTelephone);
-        fieldValues.put("customerServicesEmail", customerServicesEmail);
-
-        when(templateHelper.getCommonMapFieldValues(caseDetails)).thenReturn(fieldValues);
         when(caseDetails.getCaseData()).thenReturn(asylumCase);
 
         Map<String, Object> templateValues = updatedTribunalDecisionAndReasonsCoverLetterTemplate.mapFieldValues(caseDetails);
 
-        assertEquals(templateValues.size(), 9);
         assertEquals(templateValues.get("hmcts"), "[userImage:hmcts.png]");
-        assertEquals(templateValues.get("appealReferenceNumber"), someAppealReferenceNumber);
-        assertEquals(templateValues.get("homeOfficeReferenceNumber"), someHomeOfficeReferenceNumber);
-        assertEquals(templateValues.get("legalRepReferenceNumber"), someLegalRepReferenceNumber);
-        assertEquals(templateValues.get("appellantGivenNames"), someGivenNames);
-        assertEquals(templateValues.get("appellantFamilyName"), someFamilyName);
-        if (updatedAppealDecisionn.equals("Allowed")) {
-            assertEquals(templateValues.get("allowed"), "Yes");
-        } else {
-            assertEquals(templateValues.get("allowed"), "No");
-        }
-        assertEquals(customerServicesTelephone, fieldValues.get("customerServicesTelephone"));
-        assertEquals(customerServicesEmail, fieldValues.get("customerServicesEmail"));
+        verify(templateHelper, times(1)).getCommonMapFieldValues(caseDetails);
     }
 
     @Test
