584b759c32fe4abe12a447945ac6ffbb88ac56d7
JamieClarkeV1 <120718578+JamieClarkeV1@users.noreply.github.com>
2024-03-19 14:59:26 +0000

    RIA-8210 - Updating Document Tag post update tribunal event

--- src/main/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/entities/AsylumCaseDefinition.java
+++ src/main/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/entities/AsylumCaseDefinition.java
@@ -488,6 +488,9 @@ public enum AsylumCaseDefinition {
     CORRECTED_DECISION_AND_REASONS(
             "correctedDecisionAndReasons", new TypeReference<List<IdValue<DecisionAndReasons>>>(){}),
 
+    DECISION_AND_REASON_DOCS_UPLOAD(
+        "decisionAndReasonDocsUpload", new TypeReference<Document>(){}),
+
     ;
 
 
