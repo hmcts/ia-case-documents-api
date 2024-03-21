888e5f728b4dd22e9ee8efa132e1bab829a5a69d
Augustine Onubeze <ikechukwu.onubeze@gmail.com>
2024-03-21 15:52:38 +0000

    Fix issues in functional tests

--- src/functionalTest/java/uk/gov/hmcts/reform/iacasedocumentsapi/CcdScenarioRunnerTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/iacasedocumentsapi/CcdScenarioRunnerTest.java
@@ -13,7 +13,6 @@ import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -113,8 +112,10 @@ public class CcdScenarioRunnerTest {
         System.out.println((char) 27 + "[36m" + "-------------------------------------------------------------------");
 
         for (String scenarioSource : scenarioSources.values()) {
-            for (int i = 0; i < 3; i++) {
+            final int maxRetries = 3;
+            for (int i = 0; i < maxRetries; i++) {
 
+                String description = "";
                 try {
                     Map<String, Object> scenario = deserializeWithExpandedValues(scenarioSource);
 
