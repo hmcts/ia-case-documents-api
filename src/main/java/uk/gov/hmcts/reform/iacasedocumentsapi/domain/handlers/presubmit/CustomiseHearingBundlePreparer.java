461c8cd0c946dafcdf86adea98583a7c816fd9bb
Neha Aggarwal <neha.aggarwal@version1.com>
2024-06-03 21:25:20 +0100

    RIA-8846 Update the preparer and handler for 'Customise Hearing Bundle' for the remitted appeal
    - Updated the logic to only initialise and cusomise certain collections
      when the appeal has gone through Remittal Path.

--- src/main/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/handlers/presubmit/CustomiseHearingBundlePreparer.java
+++ src/main/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/handlers/presubmit/CustomiseHearingBundlePreparer.java
@@ -61,14 +61,83 @@ public class CustomiseHearingBundlePreparer implements PreSubmitCallbackHandler<
 
     public void prepareCustomDocuments(AsylumCase asylumCase, boolean isCaseReheard) {
         boolean isOrWasAda = asylumCase.read(SUITABILITY_REVIEW_DECISION).isPresent();
-        getMappingFields(isCaseReheard, isOrWasAda).forEach((sourceField,targetField)  ->
+        boolean isRemittedPath = asylumCase.read(SOURCE_OF_REMITTAL, String.class).isPresent();
+        getMappingFields(isCaseReheard, isOrWasAda, isRemittedPath).forEach((sourceField,targetField)  ->
             populateCustomCollections(asylumCase, sourceField, targetField)
         );
-
         // Map does not accept duplicate keys, so need to process this separately
         if (isCaseReheard) {
             populateCustomCollections(asylumCase,ADDENDUM_EVIDENCE_DOCUMENTS, CUSTOM_RESP_ADDENDUM_EVIDENCE_DOCS);
+            if (isRemittedPath) {
+                asylumCase.write(CUSTOM_LATEST_REMITTAL_DOCS, fetchLatestRemittalDocuments(asylumCase));
+                asylumCase.write(CUSTOM_REHEARD_HEARING_DOCS, fetchLatestReheardDocuments(asylumCase));
+                asylumCase.write(CUSTOM_FINAL_DECISION_AND_REASONS_DOCS, fetchLatestDecisionDocuments(asylumCase));
+            }
         }
+    }
+
+    private List<IdValue<DocumentWithDescription>> fetchLatestDecisionDocuments(AsylumCase asylumCase) {
+        //Checking if we have some documents in Collection(Object) field, else get the updated/overwritten document from the FINAL field.
+        Optional<List<IdValue<ReheardHearingDocuments>>> maybeExistingReheardDocuments =
+            asylumCase.read(REHEARD_DECISION_REASONS_COLLECTION);
+        List<IdValue<ReheardHearingDocuments>> allReheardDecisionDocuments = maybeExistingReheardDocuments
+            .orElse(emptyList());
+
+        if (allReheardDecisionDocuments.isEmpty()) {
+            Optional<List<IdValue<DocumentWithMetadata>>> finalDEcisionAndResonDocs = asylumCase.read(FINAL_DECISION_AND_REASONS_DOCUMENTS);
+            List<IdValue<DocumentWithDescription>> maybeFinalDecisionAndReasonsDocuments =
+                getDocumentWithDescListFromMetaData(finalDEcisionAndResonDocs.orElse(emptyList()));
+            return maybeFinalDecisionAndReasonsDocuments;
+        } else {
+            return getDocumentWithDescListFromMetaData(allReheardDecisionDocuments.get(0).getValue().getReheardHearingDocs());
+        }
+    }
+
+    private List<IdValue<DocumentWithDescription>> fetchLatestRemittalDocuments(AsylumCase asylumCase) {
+        Optional<List<IdValue<RemittalDocument>>> maybeExistingRemittalDocuments =
+            asylumCase.read(REMITTAL_DOCUMENTS);
+        List<IdValue<RemittalDocument>> allRemittalDocuments = maybeExistingRemittalDocuments
+            .orElse(emptyList());
+
+        if (!allRemittalDocuments.isEmpty()) {
+            RemittalDocument remittalDocument = allRemittalDocuments.get(0).getValue();
+            DocumentWithDescription remittalDocWithDesc = getDocumentWithDescFromMetaData(remittalDocument.getDecisionDocument());
+
+            List<IdValue<DocumentWithDescription>> remittalOtherDocsWithDesc = getDocumentWithDescListFromMetaData(remittalDocument.getOtherRemittalDocs());
+
+            List<IdValue<DocumentWithDescription>> allDocuments =
+                documentWithDescriptionAppender.append(remittalDocWithDesc,
+                    remittalOtherDocsWithDesc
+                );
+            return allDocuments;
+        }
+        return emptyList();
+    }
+
+    private DocumentWithDescription getDocumentWithDescFromMetaData(DocumentWithMetadata documentWithMetadata) {
+        return new DocumentWithDescription(documentWithMetadata.getDocument(),
+            documentWithMetadata.getDescription());
+    }
+
+    private List<IdValue<DocumentWithDescription>> getDocumentWithDescListFromMetaData(List<IdValue<DocumentWithMetadata>> listDocumentWithMetaData) {
+        List<IdValue<DocumentWithDescription>> listDocumentWithDesc = new ArrayList<>();
+        for (IdValue<DocumentWithMetadata> documentWithMetadataIdValue : listDocumentWithMetaData) {
+            listDocumentWithDesc = documentWithDescriptionAppender.append(
+                getDocumentWithDescFromMetaData(documentWithMetadataIdValue.getValue()), listDocumentWithDesc);
+        }
+        return listDocumentWithDesc;
+    }
+
+    private List<IdValue<DocumentWithDescription>> fetchLatestReheardDocuments(AsylumCase asylumCase) {
+        Optional<List<IdValue<ReheardHearingDocuments>>> maybeExistingReheardDocuments =
+            asylumCase.read(REHEARD_HEARING_DOCUMENTS_COLLECTION);
+        List<IdValue<ReheardHearingDocuments>> allReheardHearingDocuments = maybeExistingReheardDocuments
+            .orElse(emptyList());
+
+        if (!allReheardHearingDocuments.isEmpty()) {
+            return getDocumentWithDescListFromMetaData(allReheardHearingDocuments.get(0).getValue().getReheardHearingDocs());
+        }
+        return emptyList();
     }
 
     void populateCustomCollections(AsylumCase asylumCase, AsylumCaseDefinition sourceField, AsylumCaseDefinition targetField) {
@@ -116,17 +185,24 @@ public class CustomiseHearingBundlePreparer implements PreSubmitCallbackHandler<
 
     }
 
-    private Map<AsylumCaseDefinition,AsylumCaseDefinition> getMappingFields(boolean isReheardCase, boolean isOrWasAda) {
+    private Map<AsylumCaseDefinition,AsylumCaseDefinition> getMappingFields(boolean isReheardCase, boolean isOrWasAda, boolean isRemittedFeature) {
         Map<AsylumCaseDefinition, AsylumCaseDefinition> fieldMapping;
         if (isReheardCase) {
-            fieldMapping = new HashMap<>(Map.of(
-                APP_ADDITIONAL_EVIDENCE_DOCS, CUSTOM_APP_ADDITIONAL_EVIDENCE_DOCS,
-                RESP_ADDITIONAL_EVIDENCE_DOCS, CUSTOM_RESP_ADDITIONAL_EVIDENCE_DOCS,
-                FTPA_APPELLANT_DOCUMENTS, CUSTOM_FTPA_APPELLANT_DOCS,
-                FTPA_RESPONDENT_DOCUMENTS, CUSTOM_FTPA_RESPONDENT_DOCS,
-                FINAL_DECISION_AND_REASONS_DOCUMENTS, CUSTOM_FINAL_DECISION_AND_REASONS_DOCS,
-                REHEARD_HEARING_DOCUMENTS, CUSTOM_REHEARD_HEARING_DOCS,
-                ADDENDUM_EVIDENCE_DOCUMENTS, CUSTOM_APP_ADDENDUM_EVIDENCE_DOCS));
+            if (isRemittedFeature) {
+                fieldMapping = new HashMap<>(Map.of(
+                    FTPA_APPELLANT_DOCUMENTS, CUSTOM_FTPA_APPELLANT_DOCS,
+                    REHEARD_HEARING_DOCUMENTS, CUSTOM_REHEARD_HEARING_DOCS,
+                    ADDENDUM_EVIDENCE_DOCUMENTS, CUSTOM_APP_ADDENDUM_EVIDENCE_DOCS));
+            } else {
+                fieldMapping = new HashMap<>(Map.of(
+                    APP_ADDITIONAL_EVIDENCE_DOCS, CUSTOM_APP_ADDITIONAL_EVIDENCE_DOCS,
+                    RESP_ADDITIONAL_EVIDENCE_DOCS, CUSTOM_RESP_ADDITIONAL_EVIDENCE_DOCS,
+                    FTPA_APPELLANT_DOCUMENTS, CUSTOM_FTPA_APPELLANT_DOCS,
+                    FTPA_RESPONDENT_DOCUMENTS, CUSTOM_FTPA_RESPONDENT_DOCS,
+                    FINAL_DECISION_AND_REASONS_DOCUMENTS, CUSTOM_FINAL_DECISION_AND_REASONS_DOCS,
+                    REHEARD_HEARING_DOCUMENTS, CUSTOM_REHEARD_HEARING_DOCS,
+                    ADDENDUM_EVIDENCE_DOCUMENTS, CUSTOM_APP_ADDENDUM_EVIDENCE_DOCS));
+            }
 
         } else {
             fieldMapping = new HashMap<>(Map.of(
--- src/test/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/handlers/presubmit/CustomiseHearingBundlePreparerTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/handlers/presubmit/CustomiseHearingBundlePreparerTest.java
@@ -20,6 +20,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -60,7 +61,7 @@ class CustomiseHearingBundlePreparerTest {
     private Appender<DocumentWithDescription> appender;
     @Mock
     private FeatureToggler featureToggler;
-
+    @Mock private Document document;
     @Captor
     private ArgumentCaptor<DocumentWithDescription> documentsCaptor;
 
@@ -393,6 +394,82 @@ class CustomiseHearingBundlePreparerTest {
     }
 
     @Test
+    void should_create_custom_collections_in_reheard_case_with_remittal_enabled() {
+        when(featureToggler.getValue("reheard-feature", false)).thenReturn(true);
+        when(asylumCase.read(CASE_FLAG_SET_ASIDE_REHEARD_EXISTS, YesOrNo.class))
+            .thenReturn(Optional.of(YesOrNo.YES));
+        when(asylumCase.read(SOURCE_OF_REMITTAL, String.class)).thenReturn(Optional.of("Court of Appeal"));
+        assertEquals(asylumCase.read(CASE_FLAG_SET_ASIDE_REHEARD_EXISTS, YesOrNo.class),
+            Optional.of(YesOrNo.YES));
+
+        when(callback.getEvent()).thenReturn(Event.CUSTOMISE_HEARING_BUNDLE);
+
+        final List<IdValue<DocumentWithDescription>> customDocumentList =
+            asList(new IdValue("1", createDocumentWithDescription()));
+
+        final List<IdValue<DocumentWithMetadata>> hearingDocumentList =
+            asList(new IdValue("1", createDocumentWithMetadata(DocumentTag.REHEARD_HEARING_NOTICE, "test")));
+        final List<IdValue<DocumentWithMetadata>> ftpaAppellantEvidenceDocumentList =
+            asList(new IdValue("1", createDocumentWithMetadata(DocumentTag.ADDITIONAL_EVIDENCE, "")));
+        final List<IdValue<DocumentWithMetadata>> ftpaRespondentEvidenceDocumentList =
+            asList(new IdValue("1", createDocumentWithMetadata(DocumentTag.ADDITIONAL_EVIDENCE, "")));
+        final List<IdValue<DocumentWithMetadata>> ftpaAppellantDocumentList =
+            asList(new IdValue("1", createDocumentWithMetadata(DocumentTag.FTPA_APPELLANT, "test")));
+        final List<IdValue<DocumentWithMetadata>> ftpaRespondentDocumentList =
+            asList(new IdValue("1", createDocumentWithMetadata(DocumentTag.FTPA_RESPONDENT, "test")));
+        final List<IdValue<DocumentWithMetadata>> finalDecisionAndReasonsDocumentList =
+            asList(new IdValue("1", createDocumentWithMetadata(DocumentTag.FINAL_DECISION_AND_REASONS_PDF, "test")));
+
+        final List<IdValue<DocumentWithMetadata>> addendumEvidenceDocumentList = asList(
+            new IdValue("3", createDocumentWithMetadata(DocumentTag.ADDENDUM_EVIDENCE, "")),
+            new IdValue("2", createDocumentWithMetadata(DocumentTag.ADDENDUM_EVIDENCE, "The appellant")),
+            new IdValue("1", createDocumentWithMetadata(DocumentTag.ADDENDUM_EVIDENCE, "The respondent")));
+
+        final List<IdValue<ReheardHearingDocuments>> reheardHearingDocs = buildReheardDocuments();
+        final List<IdValue<ReheardHearingDocuments>> reheardDecisionDocs = buildReheardDocuments();
+        final List<IdValue<RemittalDocument>> remittalDocuments = buildRemittalDocuments();
+        when(asylumCase.read(REHEARD_DECISION_REASONS_COLLECTION)).thenReturn(Optional.of(reheardDecisionDocs));
+        when(asylumCase.read(REHEARD_HEARING_DOCUMENTS_COLLECTION)).thenReturn(Optional.of(reheardHearingDocs));
+        when(asylumCase.read(REMITTAL_DOCUMENTS)).thenReturn(Optional.of(remittalDocuments));
+
+        when(appender.append(any(DocumentWithDescription.class), anyList()))
+            .thenReturn(customDocumentList);
+
+        when(asylumCase.read(APP_ADDITIONAL_EVIDENCE_DOCS))
+            .thenReturn(Optional.of(ftpaAppellantEvidenceDocumentList));
+
+        when(asylumCase.read(RESP_ADDITIONAL_EVIDENCE_DOCS))
+            .thenReturn(Optional.of(ftpaRespondentEvidenceDocumentList));
+
+        when(asylumCase.read(AsylumCaseDefinition.FTPA_APPELLANT_DOCUMENTS))
+            .thenReturn(Optional.of(ftpaAppellantDocumentList));
+
+        when(asylumCase.read(AsylumCaseDefinition.FTPA_RESPONDENT_DOCUMENTS))
+            .thenReturn(Optional.of(ftpaRespondentDocumentList));
+
+        // These reheard documents will be stored in collection of objects format after Remittal feature is released.
+        when(asylumCase.read(AsylumCaseDefinition.FINAL_DECISION_AND_REASONS_DOCUMENTS))
+            .thenReturn(Optional.empty());
+
+        when(asylumCase.read(AsylumCaseDefinition.REHEARD_HEARING_DOCUMENTS))
+            .thenReturn(Optional.empty());
+
+        when(asylumCase.read(AsylumCaseDefinition.ADDENDUM_EVIDENCE_DOCUMENTS))
+            .thenReturn(Optional.of(addendumEvidenceDocumentList));
+
+
+        customiseHearingBundlePreparer.handle(PreSubmitCallbackStage.ABOUT_TO_START, callback);
+
+        verify(asylumCase).write(CUSTOM_REHEARD_HEARING_DOCS, customDocumentList);
+        verify(asylumCase).write(CUSTOM_FTPA_APPELLANT_DOCS, customDocumentList);
+        verify(asylumCase).write(CUSTOM_FINAL_DECISION_AND_REASONS_DOCS, customDocumentList);
+        verify(asylumCase).write(CUSTOM_APP_ADDENDUM_EVIDENCE_DOCS, customDocumentList);
+        verify(asylumCase).write(CUSTOM_LATEST_REMITTAL_DOCS, customDocumentList);
+        verify(asylumCase,times(1)).write(AsylumCaseDefinition.CUSTOM_RESP_ADDENDUM_EVIDENCE_DOCS,customDocumentList);
+        verify(asylumCase,times(4)).read(AsylumCaseDefinition.ADDENDUM_EVIDENCE_DOCUMENTS);
+    }
+
+    @Test
     void it_can_handle_callback() {
 
         for (Event event : Event.values()) {
@@ -457,5 +534,42 @@ class CustomiseHearingBundlePreparerTest {
                 "some-description",
                 new SystemDateProvider().now().toString(), documentTag, suppliedBy);
 
+    }
+
+    private List<IdValue<RemittalDocument>> buildRemittalDocuments() {
+
+        final DocumentWithMetadata remittalDec = new DocumentWithMetadata(
+            document, "test","2023-12-12", DocumentTag.REMITTAL_DECISION, "");
+        final DocumentWithMetadata remittalOtherDoc1 = new DocumentWithMetadata(
+            document, "other-test-1","2023-12-12", DocumentTag.REMITTAL_DECISION, "");
+        final DocumentWithMetadata remittalOtherDoc2 = new DocumentWithMetadata(
+            document, "other-test-1","2023-12-12", DocumentTag.REMITTAL_DECISION, "");
+        IdValue<DocumentWithMetadata> decisionDocWithMetadata =
+            new IdValue<>("11", remittalOtherDoc1);
+        IdValue<DocumentWithMetadata> coverLetterDocWithMetadata =
+            new IdValue<>("12", remittalOtherDoc2);
+
+        final List<IdValue<DocumentWithMetadata>> listOfDocumentsWithMetadata = Lists.newArrayList(decisionDocWithMetadata, coverLetterDocWithMetadata);
+        IdValue<RemittalDocument> remittalDocuments =
+            new IdValue<>("1", new RemittalDocument(remittalDec, listOfDocumentsWithMetadata));
+        return Lists.newArrayList(remittalDocuments);
+    }
+
+    private List<IdValue<ReheardHearingDocuments>> buildReheardDocuments() {
+        String description = "Some evidence";
+        String dateUploaded = "2018-12-25";
+        final DocumentWithMetadata coverLetterDocumentWithMetadata = new DocumentWithMetadata(
+            document, description, dateUploaded, DocumentTag.DECISION_AND_REASONS_COVER_LETTER, "");
+        final DocumentWithMetadata decisionDocumentWithMetadata = new DocumentWithMetadata(
+            document, description, dateUploaded, DocumentTag.FINAL_DECISION_AND_REASONS_PDF, "");
+
+        IdValue<DocumentWithMetadata> decisionDocWithMetadata =
+            new IdValue<>("2", decisionDocumentWithMetadata);
+        IdValue<DocumentWithMetadata> coverLetterDocWithMetadata =
+            new IdValue<>("1", coverLetterDocumentWithMetadata);
+        final List<IdValue<DocumentWithMetadata>> listOfDocumentsWithMetadata = Lists.newArrayList(decisionDocWithMetadata, coverLetterDocWithMetadata);
+        IdValue<ReheardHearingDocuments> reheardHearingDocuments =
+            new IdValue<>("1", new ReheardHearingDocuments(listOfDocumentsWithMetadata));
+        return Lists.newArrayList(reheardHearingDocuments);
     }
 }
