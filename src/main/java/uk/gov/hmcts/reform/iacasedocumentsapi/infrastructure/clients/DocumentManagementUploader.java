99fd5c9ad2dc5e36f9ec77a8d6fae7370ffa3031
apereverzin1 <140609226+apereverzin1@users.noreply.github.com>
2024-08-06 12:53:57 +0100

    DIAC-555 add logs (#934)
    * Added logs
    
    * Added logs

--- src/main/java/uk/gov/hmcts/reform/iacasedocumentsapi/infrastructure/clients/DocumentManagementUploader.java
+++ src/main/java/uk/gov/hmcts/reform/iacasedocumentsapi/infrastructure/clients/DocumentManagementUploader.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.iacasedocumentsapi.infrastructure.clients;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.ccd.field.Document;
@@ -9,6 +10,7 @@ import uk.gov.hmcts.reform.iacasedocumentsapi.domain.service.FeatureToggler;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class DocumentManagementUploader implements DocumentUploader {
 
     private final FeatureToggler featureToggler;
@@ -18,8 +20,10 @@ public class DocumentManagementUploader implements DocumentUploader {
     @Override
     public Document upload(Resource resource, String contentType) {
         if (featureToggler.getValue("use-ccd-document-am", false)) {
+            log.info("Uploading {} using CDAM", resource.getFilename());
             return cdamDocumentManagementUploader.upload(resource, contentType);
         } else {
+            log.info("Uploading {} not using CDAM", resource.getFilename());
             return dmDocumentManagementUploader.upload(resource, contentType);
         }
     }
