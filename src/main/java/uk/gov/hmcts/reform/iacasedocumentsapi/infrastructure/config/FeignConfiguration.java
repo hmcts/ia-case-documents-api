f41bc28c39400d84ab491216f7750eb5af659af4
nilay <nilay.patel@solirius.com>
2025-06-03 09:43:28 +0100

    fix?

--- src/main/java/uk/gov/hmcts/reform/iacasedocumentsapi/infrastructure/config/FeignConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/iacasedocumentsapi/infrastructure/config/FeignConfiguration.java
@@ -1,29 +1,13 @@
 package uk.gov.hmcts.reform.iacasedocumentsapi.infrastructure.config;
 
-import static com.fasterxml.jackson.databind.DeserializationFeature.READ_ENUMS_USING_TO_STRING;
-import static com.fasterxml.jackson.databind.DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_USING_DEFAULT_VALUE;
-import static com.fasterxml.jackson.databind.SerializationFeature.WRITE_ENUMS_USING_TO_STRING;
-
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.MapperFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import feign.codec.Decoder;
 import feign.codec.Encoder;
 import feign.form.spring.SpringFormEncoder;
 import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
-import org.springframework.cloud.openfeign.support.ResponseEntityDecoder;
-import org.springframework.cloud.openfeign.support.SpringDecoder;
 import org.springframework.cloud.openfeign.support.SpringEncoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
 @Configuration
 public class FeignConfiguration {
@@ -34,27 +18,5 @@ public class FeignConfiguration {
         ObjectFactory<HttpMessageConverters> messageConverters
     ) {
         return new SpringFormEncoder(new SpringEncoder(messageConverters));
-    }
-
-    @Bean
-    public Decoder decoder() {
-        HttpMessageConverter<Object> jacksonConverter = new MappingJackson2HttpMessageConverter(objectMapper());
-
-        return new ResponseEntityDecoder(new SpringDecoder(() -> new HttpMessageConverters(jacksonConverter)));
-    }
-
-    public ObjectMapper objectMapper() {
-        ObjectMapper objectMapper = new Jackson2ObjectMapperBuilder()
-            .featuresToEnable(READ_ENUMS_USING_TO_STRING)
-            .featuresToEnable(READ_UNKNOWN_ENUM_VALUES_USING_DEFAULT_VALUE)
-            .featuresToEnable(WRITE_ENUMS_USING_TO_STRING)
-            .featuresToDisable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
-            .featuresToDisable(MapperFeature.REQUIRE_HANDLERS_FOR_JAVA8_TIMES)
-            .serializationInclusion(JsonInclude.Include.NON_ABSENT)
-            .createXmlMapper(false)
-            .build();
-        objectMapper.registerModule(new Jdk8Module());
-        objectMapper.registerModule(new JavaTimeModule());
-        return objectMapper;
     }
 }
