56906c0a42b7a7f425f7a1519405591aa82a9e53
Andrey Pereverzin <andreypereverzin@Mac.broadband>
2025-06-20 13:04:49 +0100

    DIAC-707 Logs

--- src/main/java/uk/gov/hmcts/reform/iacasedocumentsapi/infrastructure/controllers/PreSubmitCallbackController.java
+++ src/main/java/uk/gov/hmcts/reform/iacasedocumentsapi/infrastructure/controllers/PreSubmitCallbackController.java
@@ -6,14 +6,21 @@ import static org.springframework.http.ResponseEntity.ok;
 
 import io.swagger.v3.oas.annotations.Parameter;
 import javax.validation.constraints.NotNull;
+
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
+import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.AsylumCase;
 import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.ccd.CaseData;
+import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.ccd.CaseDetails;
 import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.ccd.callback.Callback;
 import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.ccd.callback.PreSubmitCallbackResponse;
 import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.ccd.callback.PreSubmitCallbackStage;
 import uk.gov.hmcts.reform.iacasedocumentsapi.infrastructure.PreSubmitCallbackDispatcher;
 
+import java.util.Optional;
+
+@Slf4j
 public class PreSubmitCallbackController<T extends CaseData> {
 
     private static final org.slf4j.Logger LOG = getLogger(PreSubmitCallbackController.class);
@@ -31,12 +38,38 @@ public class PreSubmitCallbackController<T extends CaseData> {
     public ResponseEntity<PreSubmitCallbackResponse<T>> ccdAboutToStart(
         @Parameter(name = "Asylum case data", required = true) @NotNull @RequestBody Callback<T> callback
     ) {
+        log.info("------------------ccdAboutToStart111");
+        log.info("{}", callback.getCaseDetails().getCaseData());
+        log.info("------------------ccdAboutToStart222");
+        Optional<CaseDetails<T>> c = callback.getCaseDetailsBefore();
+        if (c.isPresent()) {
+            AsylumCase asylumCaseDataBefore = (AsylumCase)c.get().getCaseData();
+            log.info("----------asylumCaseDataBefore111");
+            log.info("{}", asylumCaseDataBefore);
+            log.info("----------asylumCaseDataBefore222");
+        } else {
+            log.info("----------asylumCaseDataBefore is not present");
+        }
+        log.info("------------------ccdAboutToStart333");
         return performStageRequest(PreSubmitCallbackStage.ABOUT_TO_START, callback);
     }
 
     public ResponseEntity<PreSubmitCallbackResponse<T>> ccdAboutToSubmit(
         Callback<T> callback
     ) {
+        log.info("------------------ccdAboutToSubmit111");
+        log.info("{}", callback.getCaseDetails().getCaseData());
+        log.info("------------------ccdAboutToSubmit222");
+        Optional<CaseDetails<T>> c = callback.getCaseDetailsBefore();
+        if (c.isPresent()) {
+            AsylumCase asylumCaseDataBefore = (AsylumCase)c.get().getCaseData();
+            log.info("----------asylumCaseDataBefore111");
+            log.info("{}", asylumCaseDataBefore);
+            log.info("----------asylumCaseDataBefore222");
+        } else {
+            log.info("----------asylumCaseDataBefore is not present");
+        }
+        log.info("------------------ccdAboutToSubmit333");
         return performStageRequest(PreSubmitCallbackStage.ABOUT_TO_SUBMIT, callback);
     }
 
