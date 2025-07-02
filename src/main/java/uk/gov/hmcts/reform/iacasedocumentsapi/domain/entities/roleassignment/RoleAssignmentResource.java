711cd428abe742aff665432654e61ba76ce5c67a
nilay <nilay.patel@solirius.com>
2025-07-02 14:44:17 +0100

    checkstyle fix

--- src/main/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/entities/roleassignment/RoleAssignmentResource.java
+++ src/main/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/entities/roleassignment/RoleAssignmentResource.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
-import lombok.EqualsAndHashCode;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
