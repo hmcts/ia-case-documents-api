11258e8cd6a50ff78bba7cb8113438fab47c1bd5
Sabah Irfan <sabah.irfan@gmail.com>
2025-09-08 10:11:40 +0100

    Fix failing tests

--- src/test/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/entities/DocumentTagTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/entities/DocumentTagTest.java
@@ -106,6 +106,6 @@ class DocumentTagTest {
 
     @Test
     public void if_this_test_fails_it_is_because_it_needs_updating_with_your_changes() {
-        assertEquals(102, DocumentTag.values().length);
+        assertEquals(103, DocumentTag.values().length);
     }
 }
