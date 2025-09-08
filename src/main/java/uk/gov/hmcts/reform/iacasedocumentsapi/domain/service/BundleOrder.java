77fa6f02eedda3d428c91a2ed479a3845a3d89e3
Sabah u Din Irfan <sabah.irfan@gmail.com>
2025-09-08 19:07:08 +0100

    DIAC-1378 Late appeal can proceed - Detained (in IRC or Prison) notification (#1047)

--- src/main/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/service/BundleOrder.java
+++ src/main/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/service/BundleOrder.java
@@ -313,7 +313,11 @@ public class BundleOrder implements Comparator<DocumentWithMetadata> {
                 log.warn("INTERNAL_DETAINED_PRISON_IRC_APPEAL_SUBMISSION tag should not be checked for bundle ordering, document desc: {}", document.getDescription());
                 yield 92;
             }
-            case NONE ->  93;
+            case INTERNAL_DETAINED_OUT_OF_TIME_DECISION_ALLOWED_LETTER -> {
+                log.warn("INTERNAL_DETAINED_OUT_OF_TIME_DECISION_ALLOWED_LETTER tag should not be checked for bundle ordering, document desc: {}", document.getDescription());
+                yield 93;
+            }
+            case NONE ->  94;
             default ->
                 throw new IllegalStateException("document has unknown tag: " + document.getTag() + ", description: " + document.getDescription());
         };
