e57281ee55ee6413b6929d571d4ffdb7a637a84d
nilay <nilay.patel@solirius.com>
2025-10-06 15:55:43 +0100

    checsktyle

--- src/main/java/uk/gov/hmcts/reform/iacasedocumentsapi/infrastructure/clients/roleassignment/RoleAssignmentApi.java
+++ src/main/java/uk/gov/hmcts/reform/iacasedocumentsapi/infrastructure/clients/roleassignment/RoleAssignmentApi.java
@@ -1,13 +1,9 @@
 package uk.gov.hmcts.reform.iacasedocumentsapi.infrastructure.clients.roleassignment;
 
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
-import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static uk.gov.hmcts.reform.ccd.document.am.feign.CaseDocumentClientApi.SERVICE_AUTHORIZATION;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
