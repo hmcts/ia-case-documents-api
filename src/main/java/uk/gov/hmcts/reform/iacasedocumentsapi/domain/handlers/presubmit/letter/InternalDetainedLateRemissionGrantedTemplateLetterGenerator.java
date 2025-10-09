0fe6e37f347ed3b3018fcf8eccb10b704b928578
rajeshthuraiyur <48379561+rajeshthuraiyur@users.noreply.github.com>
2025-10-09 15:21:20 +0100

    DIAC-1326 changes to enable remission letter generation (#1093)
    * Update InternalDetainedAppealRemissionGrantedInTimeLetterGenerator.java
    
    * updates to fix check style issues
    
    * updates to fix payment status for remission
    
    * updates to fix payment status condition for remission decission
    
    * updates to fix payment status condition for remission decission
    
    * updates to fix payment status condition for remission decission
    
    * updates to fix payment status condition for remission decission
    
    * updates to fix payment status condition for remission decission
    
    * updates to fix late remission scenarios
    
    ---------
    
    Co-authored-by: Rajesh Thuraiyur Moorthy <rajeshthuraiyurmoorthy@Mac.powerhub>

--- src/main/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/handlers/presubmit/letter/InternalDetainedLateRemissionGrantedTemplateLetterGenerator.java
+++ src/main/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/handlers/presubmit/letter/InternalDetainedLateRemissionGrantedTemplateLetterGenerator.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.AsylumCase;
 import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.AsylumCaseDefinition;
 import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.DocumentTag;
+import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.RemissionType;
 import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.ccd.CaseDetails;
 import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.ccd.Event;
 import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.ccd.callback.Callback;
@@ -17,7 +18,9 @@ import uk.gov.hmcts.reform.iacasedocumentsapi.domain.service.DocumentCreator;
 import uk.gov.hmcts.reform.iacasedocumentsapi.domain.service.DocumentHandler;
 
 import java.util.Objects;
+import java.util.Optional;
 
+import static uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.AsylumCaseDefinition.LATE_REMISSION_TYPE;
 import static uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.AsylumCaseDefinition.NOTIFICATION_ATTACHMENT_DOCUMENTS;
 import static uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.DetentionFacility.IRC;
 import static uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.DetentionFacility.PRISON;
@@ -53,13 +56,16 @@ public class InternalDetainedLateRemissionGrantedTemplateLetterGenerator impleme
                 .read(AsylumCaseDefinition.PAYMENT_STATUS, PaymentStatus.class)
                 .map(paymentStatus -> paymentStatus == PAID).orElse(false);
 
+        Optional<RemissionType> lateRemissionType = asylumCase.read(LATE_REMISSION_TYPE, RemissionType.class);
+
         return callbackStage == PreSubmitCallbackStage.ABOUT_TO_SUBMIT
                 && callback.getEvent() == Event.RECORD_REMISSION_DECISION
                 && hasBeenSubmittedByAppellantInternalCase(asylumCase)
                 && paymentPaid
                 && remissionDecisionGranted(asylumCase)
                 && isDetainedInOneOfFacilityTypes(asylumCase, IRC, PRISON)
-                && !isAcceleratedDetainedAppeal(asylumCase);
+                && !isAcceleratedDetainedAppeal(asylumCase)
+                && lateRemissionType.isPresent();
     }
 
     public PreSubmitCallbackResponse<AsylumCase> handle(
