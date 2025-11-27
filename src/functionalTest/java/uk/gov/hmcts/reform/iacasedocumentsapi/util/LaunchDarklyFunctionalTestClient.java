a176cad9575925cd438eccd3cc6e57982922402f
nilay <nilay.patel@solirius.com>
2025-11-27 17:04:16 +0000

    adding missing files

---
+++ src/functionalTest/java/uk/gov/hmcts/reform/iacasedocumentsapi/util/LaunchDarklyFunctionalTestClient.java
@@ -0,0 +1,58 @@
+package uk.gov.hmcts.reform.iacasedocumentsapi.util;
+
+
+import com.launchdarkly.sdk.LDContext;
+import com.launchdarkly.sdk.LDUser;
+import com.launchdarkly.sdk.server.interfaces.LDClientInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientResponseException;
+import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.UserDetails;
+import uk.gov.hmcts.reform.iacasedocumentsapi.infrastructure.clients.IdamApi;
+import uk.gov.hmcts.reform.iacasedocumentsapi.infrastructure.clients.model.idam.UserInfo;
+import uk.gov.hmcts.reform.iacasedocumentsapi.infrastructure.security.idam.IdamUserDetails;
+import uk.gov.hmcts.reform.iacasedocumentsapi.infrastructure.security.idam.IdentityManagerResponseException;
+
+@Component
+public class LaunchDarklyFunctionalTestClient {
+
+    @Autowired private IdamApi idamApi;
+    @Autowired private LDClientInterface ldClient;
+
+    public boolean getKey(String key, String accessToken) {
+
+        UserDetails userDetails = getUserDetails(accessToken);
+
+        LDUser ldUser =  new LDUser.Builder(userDetails.getId())
+                .firstName(userDetails.getForename())
+                .lastName(userDetails.getSurname())
+                .email(userDetails.getEmailAddress())
+                .build();
+
+        return ldClient.boolVariation(key, LDContext.fromUser(ldUser), false);
+    }
+
+    private IdamUserDetails getUserDetails(String accessToken) {
+        try {
+
+            UserInfo userInfo = idamApi.userInfo(accessToken);
+
+            return new IdamUserDetails(
+                    accessToken,
+                    userInfo.getUid(),
+                    userInfo.getRoles(),
+                    userInfo.getEmail(),
+                    userInfo.getGivenName(),
+                    userInfo.getFamilyName()
+            );
+
+        } catch (RestClientResponseException ex) {
+
+            throw new IdentityManagerResponseException(
+                    "Could not get user details with IDAM",
+                    ex
+            );
+        }
+    }
+}
+
