4942dc40b9c61c3b46ee2dbd45b19ba40f548634
nilay <nilay.patel@solirius.com>
2025-11-28 13:43:35 +0000

    deploy issue potential fix

--- src/main/java/uk/gov/hmcts/reform/iacasedocumentsapi/infrastructure/config/FeignConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/iacasedocumentsapi/infrastructure/config/FeignConfiguration.java
@@ -29,19 +29,19 @@ public class FeignConfiguration {
         return new SpringFormEncoder(new SpringEncoder(messageConverters));
     }
 
-    @Bean
-    public Decoder decoder() {
-        HttpMessageConverter jacksonConverter = new MappingJackson2HttpMessageConverter(objectMapper());
-
-        return new ResponseEntityDecoder(new SpringDecoder(() -> new HttpMessageConverters(jacksonConverter)));
-    }
-
-    public ObjectMapper objectMapper() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        objectMapper.configure(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_USING_DEFAULT_VALUE, true);
-        objectMapper.registerModule(new Jdk8Module());
-        objectMapper.registerModule(new JavaTimeModule());
-        return objectMapper;
-    }
+    //    @Bean
+    //    public Decoder decoder() {
+    //        HttpMessageConverter jacksonConverter = new MappingJackson2HttpMessageConverter(objectMapper());
+    //
+    //        return new ResponseEntityDecoder(new SpringDecoder(() -> new HttpMessageConverters(jacksonConverter)));
+    //    }
+    //
+    //    public ObjectMapper objectMapper() {
+    //        ObjectMapper objectMapper = new ObjectMapper();
+    //        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    //        objectMapper.configure(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_USING_DEFAULT_VALUE, true);
+    //        objectMapper.registerModule(new Jdk8Module());
+    //        objectMapper.registerModule(new JavaTimeModule());
+    //        return objectMapper;
+    //    }
 }
