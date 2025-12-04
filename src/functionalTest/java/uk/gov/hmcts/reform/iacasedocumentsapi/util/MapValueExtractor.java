7cb3ac48c53e49b4641de1c898c9a8d52e9528d7
nilay <nilay.patel@solirius.com>
2025-12-04 12:12:56 +0000

    fixing functional tests and map extractor and some notifications from expectations

--- src/functionalTest/java/uk/gov/hmcts/reform/iacasedocumentsapi/util/MapValueExtractor.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/iacasedocumentsapi/util/MapValueExtractor.java
@@ -35,7 +35,7 @@ public final class MapValueExtractor {
                 return null;
             }
 
-            Map expectationMap = (Map) value;
+            Map<String, Object> expectationMap = (Map<String, Object>) value;
 
             if (!featureFlag) {
 
@@ -75,12 +75,30 @@ public final class MapValueExtractor {
                     Map<String, Object> caseData = (Map<String, Object>) expectationMap.get("caseData");
                     Map<String, Object> updateCaseData = new HashMap<>(caseData);
 
-                    if (caseData.containsKey("replacements")) {
-                        Map<String, Object> replacement = (Map<String, Object>) caseData.get("replacements");
+                    if (updateCaseData.containsKey("notifications")) {
+                        List<Map<String, Object>> caseDataNotifications = (List<Map<String, Object>>) updateCaseData.get(
+                            "notifications");
+                        List<Map<String, Object>> updatedCaseDataNotifications = new ArrayList<>(caseDataNotifications);
+
+                        for (Map<String, Object> notification : caseDataNotifications) {
+                            String notificationValue = (String) notification.get("reference");
+
+                            if (notificationValue.contains("_CASE_OFFICER")) {
+                                updatedCaseDataNotifications.remove(notification);
+                            }
+                        }
+                        ((Map<String, Object>) value).remove("notifications");
+                        if (!updatedCaseDataNotifications.isEmpty()) {
+                            ((Map<String, Object>) value).put("notifications", updatedCaseDataNotifications);
+                        }
+                    }
+
+                    if (updateCaseData.containsKey("replacements")) {
+                        Map<String, Object> replacement = (Map<String, Object>) updateCaseData.get("replacements");
                         Map<String, Object> updatedReplacement = new HashMap<>(replacement);
 
                         if (replacement.containsKey("notificationsSent")) {
-                            List<Map<String, Object>> notificationsSent = (List<Map<String, Object>>) replacement.get(
+                            List<Map<String, Object>> notificationsSent = (List<Map<String, Object>>) updatedReplacement.get(
                                 "notificationsSent");
                             List<Map<String, Object>> updatedNotificationsSent = new ArrayList<>(notificationsSent);
 
@@ -92,13 +110,13 @@ public final class MapValueExtractor {
                                 }
                             }
 
-                            replacement.remove("notificationsSent");
-                            if (updatedNotificationsSent.size() > 0) {
-                                replacement.put("notificationsSent", updatedNotificationsSent);
+                            updatedReplacement.remove("notificationsSent");
+                            if (!updatedNotificationsSent.isEmpty()) {
+                                updatedReplacement.put("notificationsSent", updatedNotificationsSent);
                             }
 
-                            caseData.remove("replacements");
-                            caseData.put("replacements", updatedReplacement);
+                            updateCaseData.remove("replacements");
+                            updateCaseData.put("replacements", updatedReplacement);
 
                             ((Map<String, Object>) value).remove("caseData");
                             ((Map<String, Object>) value).put("caseData", updateCaseData);
@@ -131,27 +149,7 @@ public final class MapValueExtractor {
     }
 
     public static <T> T extract(Map<String, Object> map, String path) {
-
-        if (!path.contains(".")) {
-            return (T) map.get(path);
-        }
-
-        Map<String, Object> currentMap = map;
-
-        String[] pathParts = path.split("\\.");
-
-        for (int i = 0; i < pathParts.length - 1; i++) {
-
-            Object value = currentMap.get(pathParts[i]);
-
-            if (!(value instanceof Map)) {
-                return null;
-            }
-
-            currentMap = (Map<String, Object>) value;
-        }
-
-        return (T) currentMap.get(pathParts[pathParts.length - 1]);
+        return extract(map, path, false);
     }
 
     public static <T> T extractOrDefault(Map<String, Object> map, String path, T defaultValue, Boolean featureFlag) {
