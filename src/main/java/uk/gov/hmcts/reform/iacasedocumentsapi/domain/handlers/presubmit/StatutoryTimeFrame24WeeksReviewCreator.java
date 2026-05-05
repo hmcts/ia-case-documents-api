a2ed0e453b55798547b89d993eee13b3588e94cc
Siva Chimpiri <siva.chimpiri@hmcts.net>
2026-05-05 18:59:40 +0100

    DIAC-2146 test branch

--- src/main/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/handlers/presubmit/StatutoryTimeFrame24WeeksReviewCreator.java
+++ src/main/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/handlers/presubmit/StatutoryTimeFrame24WeeksReviewCreator.java
@@ -11,14 +11,12 @@ import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.ccd.callback.Callb
 import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.ccd.callback.PreSubmitCallbackResponse;
 import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.ccd.callback.PreSubmitCallbackStage;
 import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.ccd.field.Document;
-import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.ccd.field.YesOrNo;
 import uk.gov.hmcts.reform.iacasedocumentsapi.domain.handlers.PreSubmitCallbackHandler;
 import uk.gov.hmcts.reform.iacasedocumentsapi.domain.service.DocumentCreator;
 import uk.gov.hmcts.reform.iacasedocumentsapi.domain.service.DocumentHandler;
 
 import static java.util.Objects.requireNonNull;
 import static uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.AsylumCaseDefinition.NOTIFICATION_ATTACHMENT_DOCUMENTS;
-import static uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.ccd.field.YesOrNo.YES;
 import static uk.gov.hmcts.reform.iacasedocumentsapi.domain.utils.AsylumCaseUtils.isInternalCase;
 
 @Component
@@ -35,12 +33,6 @@ public class StatutoryTimeFrame24WeeksReviewCreator implements PreSubmitCallback
         this.documentHandler = documentHandler;
     }
 
-    private static boolean hasStf24WeeksStatus(AsylumCase asylumCase) {
-        final YesOrNo status = asylumCase
-                .read(AsylumCaseDefinition.STF_24W_CURRENT_STATUS_AUTO_GENERATED, YesOrNo.class)
-                .orElseThrow(() -> new IllegalStateException("STF 24W CURRENT STATUS AUTO GENERATED is not present"));
-        return status.equals(YES);
-    }
 
     public boolean canHandle(
             PreSubmitCallbackStage callbackStage,
@@ -57,7 +49,7 @@ public class StatutoryTimeFrame24WeeksReviewCreator implements PreSubmitCallback
         return callbackStage == PreSubmitCallbackStage.ABOUT_TO_SUBMIT
                 &&
                 Event.COMPLETE_CASE_REVIEW
-                        .equals(callback.getEvent()) && hasStf24WeeksStatus(asylumCase);
+                        .equals(callback.getEvent());
     }
 
     public PreSubmitCallbackResponse<AsylumCase> handle(
