15074f9b7b3ec1b8668c9c2b731647c09ec235d2
tiagreaney <tiagreaney@gmail.com>
2026-05-11 08:42:12 +0100

    update launch darkly key

--- src/functionalTest/java/uk/gov/hmcts/reform/iacasedocumentsapi/util/SystemDocumentManagementUploader.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/iacasedocumentsapi/util/SystemDocumentManagementUploader.java
@@ -4,13 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.iacasedocumentsapi.domain.entities.ccd.field.Document;
-import uk.gov.hmcts.reform.iacasedocumentsapi.domain.service.FeatureToggler;
 
 @Service
 @RequiredArgsConstructor
 public class SystemDocumentManagementUploader {
-
-    private final FeatureToggler featureToggler;
 
     private final CdamSystemDocumentManagementUploader cdamSystemDocumentManagementUploader;
 
