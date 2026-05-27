417c7bb93068dfd24fa8e749ddae208b3f52e997
nilay <nilay.patel@solirius.com>
2026-05-27 18:06:32 +0100

    improve

--- src/functionalTest/java/uk/gov/hmcts/reform/iacasedocumentsapi/CcdScenarioRunnerTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/iacasedocumentsapi/CcdScenarioRunnerTest.java
@@ -71,8 +71,6 @@ public class CcdScenarioRunnerTest {
     @MockitoBean
     RequestUserAccessTokenProvider requestUserAccessTokenProvider;
 
-    private Map<String, Object> actualResponse = null;
-
     private final Map<String, String> scenarioSources = new HashMap<>();
 
     @BeforeAll
@@ -199,8 +197,8 @@ public class CcdScenarioRunnerTest {
                                                      long testCaseId,
                                                      Map<String, Object> expectedResponse) throws IOException {
         assumeFalse(fileName.startsWith("Disabled:"), "Test marked as disabled");
+        Map<String, Object> responseForError = null;
         try {
-            actualResponse = null;
             String actualResponseBody =
                 SerenityRest
                     .given()
@@ -216,7 +214,8 @@ public class CcdScenarioRunnerTest {
                     .body()
                     .asString();
 
-            actualResponse = MapSerializer.deserialize(actualResponseBody);
+            Map<String, Object> actualResponse = MapSerializer.deserialize(actualResponseBody);
+            responseForError = actualResponse;
 
             verifiers.forEach(verifier ->
                 verifier.verify(
@@ -228,8 +227,8 @@ public class CcdScenarioRunnerTest {
             );
         } catch (Error | RetryableException | NullPointerException e) {
             System.out.println("Scenario failed with error " + e.getMessage());
-            if (actualResponse != null) {
-                System.out.println("actualResponse: " + objectMapper.writeValueAsString(actualResponse));
+            if (responseForError != null) {
+                System.out.println("actualResponse: " + objectMapper.writeValueAsString(responseForError));
                 System.out.println("expectedResponse: " + objectMapper.writeValueAsString(expectedResponse));
             }
             throw e;
