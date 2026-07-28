bcda70aac680bee33c2c7dd3eff266f5638f4af7
Siva Chimpiri <siva.chimpiri@hmcts.net>
2026-07-28 10:26:43 +0100

    DIAC-2469 reference numbers added

--- src/main/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/utils/Stf24WeeksUtils.java
+++ src/main/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/utils/Stf24WeeksUtils.java
@@ -39,6 +39,7 @@ public class Stf24WeeksUtils {
     public static final String APPEAL_REFERENCE_NUMBER_KEY = "appealReferenceNumber";
     public static final String HOME_OFFICE_REFERENCE_NUMBER_KEY = "homeOfficeReferenceNumber";
 
+
     private Stf24WeeksUtils() {
 
     }
