10618cbad3f9edda06009d1dec6f715ad9c84bc9
tiagreaney <tiagreaney@gmail.com>
2026-07-31 13:24:35 +0100

    update condition

--- src/main/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/service/HearingNoticeFieldMapper.java
+++ src/main/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/service/HearingNoticeFieldMapper.java
@@ -66,18 +66,17 @@ public class HearingNoticeFieldMapper {
             fieldValues.put("remoteVideoCallTribunalResponse", asylumCase.read(REMOTE_VIDEO_CALL_TRIBUNAL_RESPONSE, String.class).orElse(""));
         }
 
-        String listCaseHearingCentre = asylumCase.read(LIST_CASE_HEARING_CENTRE, String.class).orElse("");
-        HearingCentre cmrHearingCentre = asylumCase.read(CMR_HEARING_CENTRE_ADDRESS, HearingCentre.class).orElse(null);
-        log.info("List case hearing centre address: {}", listCaseHearingCentre);
-        log.info("CMR hearing centre address: {}", cmrHearingCentre);
+        var hearingCentreField = AsylumCaseUtils.isCmrCase(asylumCase)
+                ? CMR_HEARING_CENTRE_ADDRESS
+                : LIST_CASE_HEARING_CENTRE_ADDRESS;
 
-        fieldValues.put("hearingCentreAddress",
+        fieldValues.put(
+                "hearingCentreAddress",
                 isCaseUsingLocationRefData
-                        ? asylumCase.read(
-                        AsylumCaseUtils.isCmrCase(asylumCase) ? CMR_HEARING_CENTRE_ADDRESS : LIST_CASE_HEARING_CENTRE_ADDRESS,
-                        String.class
-                ).orElse("")
-                : stringProvider.get("hearingCentreAddress", listedHearingCentre.toString()).orElse("").replaceAll(",\\s*", "\n")
+                        ? asylumCase.read(hearingCentreField, String.class).orElse("")
+                        : stringProvider.get("hearingCentreAddress", listedHearingCentre.toString())
+                        .orElse("")
+                        .replaceAll(",\\s*", "\n")
         );
 
         if (isSubmitRequirementsAvailable.isPresent() && isSubmitRequirementsAvailable.get() == YesOrNo.YES) {
