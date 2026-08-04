7c46f7193272510005823a20207d0c3522023e3b
tiagreaney <tiagreaney@gmail.com>
2026-08-04 08:24:26 +0100

    add logs

--- src/main/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/service/HearingNoticeFieldMapper.java
+++ src/main/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/service/HearingNoticeFieldMapper.java
@@ -120,6 +120,7 @@ public class HearingNoticeFieldMapper {
 
         log.info("isCmrCase: {}", isCmrCase);
         log.info("Hearing centre address: {}", hearingCentreAddress);
+        log.info("Sponsor name: {}", asylumCase.read(SPONSOR_GIVEN_NAMES, String.class).orElse(""));
         fieldValues.put("hearingCentreAddress", hearingCentreAddress);
 
         if (isSubmitRequirementsAvailable.isPresent()
