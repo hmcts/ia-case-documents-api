7bfba9912636338d8dd8bb319dd404139b0e2466
Rajesh Thuraiyur Moorthy <rajeshthuraiyurmoorthy@Rajeshs-MacBook-Pro.local>
2026-09-01 10:10:04 +0100

    updates to fix merge conflicts

--- src/test/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/entities/DocumentTagTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/entities/DocumentTagTest.java
@@ -143,6 +143,6 @@ class DocumentTagTest {
 
     @Test
     public void if_this_test_fails_it_is_because_it_needs_updating_with_your_changes() {
-        assertEquals(134, DocumentTag.values().length);
+        assertEquals(144, DocumentTag.values().length);
     }
 }
--- src/test/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/service/BundleOrderTest.java
+++ src/test/java/uk/gov/hmcts/reform/iacasedocumentsapi/domain/service/BundleOrderTest.java
@@ -38,7 +38,7 @@ class BundleOrderTest {
             .map(DocumentWithMetadata::getTag)
             .toList();
 
-        assertEquals(127, sortedTags.size());
+        assertEquals(137, sortedTags.size());
 
         List<DocumentTag> documentTagList = Arrays.asList(
             DocumentTag.CASE_SUMMARY,
